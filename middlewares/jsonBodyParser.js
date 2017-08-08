89f41b031b2b9105c7d9ecfd263d62355fe25508
Luciano Mammino <lucianomammino@gmail.com>
2017-08-08 23:07:28 +0100

    Improved error management for JSON body parser middleware

--- middlewares/jsonBodyParser.js
+++ middlewares/jsonBodyParser.js
@@ -1,7 +1,13 @@
+const createError = require('http-errors')
+
 module.exports = () => ({
   before: (ctx, next) => {
     if (ctx.event.headers['Content-Type'] === 'application/json') {
-      ctx.event.body = JSON.parse(ctx.event.body)
+      try {
+        ctx.event.body = JSON.parse(ctx.event.body)
+      } catch (err) {
+        throw new createError.UnprocessableEntity('Content type defined as JSON but an invalid JSON was provided')
+      }
     }
     next()
   }
