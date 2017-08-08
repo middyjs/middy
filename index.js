803032ce0b6ee5738890f393e97fe19a8d581ae5
Luciano Mammino <lucianomammino@gmail.com>
2017-08-08 19:16:09 +0100

    fixed error in error middlewares

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const runMiddlewares = (middlewares, ctx, done) => {
       if (err) {
         return done(err)
       }
-      
+
       const nextMiddleware = stack.shift()
 
       if (nextMiddleware) {
@@ -28,7 +28,7 @@ const runErrorMiddlewares = (middlewares, ctx, done) => {
       if (!err) {
         return done()
       }
-      
+
       const nextMiddleware = stack.shift()
 
       if (nextMiddleware) {
@@ -57,33 +57,35 @@ const middy = (handler) => {
     }
 
     instance.ctx = ctx
-    
+
     const terminate = (err) => {
       if (err) {
         return callback(err)
       }
-      
+
       return callback(null, ctx.response)
     }
 
     runMiddlewares(beforeMiddlewares, ctx, (err) => {
       if (err) {
+        ctx.error = err
         return runErrorMiddlewares(errorMiddlewares, ctx, terminate)
       }
-      
+
       handler.call(ctx, ctx.event, context, (err, response) => {
-        ctx.error = err
         ctx.response = response
-        
+
         if (err) {
+          ctx.error = err
           return runErrorMiddlewares(errorMiddlewares, ctx, terminate)
         }
-        
+
         runMiddlewares(afterMiddlewares, ctx, (err) => {
           if (err) {
+            ctx.error = err
             return runErrorMiddlewares(errorMiddlewares, ctx, terminate)
           }
-          
+
           return terminate()
         })
       })
@@ -98,29 +100,29 @@ const middy = (handler) => {
     if (middleware.after) {
       instance.after(middleware.after)
     }
-    
+
     if (middleware.error) {
       instance.error(middleware.error)
     }
 
     return instance
   }
-  
+
   instance.before = (beforeMiddleware) => {
     beforeMiddlewares.push(beforeMiddleware)
-    
+
     return instance
   }
-  
+
   instance.after = (afterMiddleware) => {
     afterMiddlewares.unshift(afterMiddleware)
-    
+
     return instance
   }
-  
+
   instance.error = (errorMiddleware) => {
     errorMiddlewares.push(errorMiddleware)
-    
+
     return instance
   }
 
