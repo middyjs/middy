0c185b316502f326519a17971d7f9dd3a51089c0
Luciano Mammino <lucianomammino@gmail.com>
2017-08-22 13:36:07 +0100

    Urlencoded body parser middleware (#18)
    * Working on urlencoded body parser middleware
    
    * Added body parser for urlencoded bodies

---
+++ src/middlewares/__tests__/urlencodedBodyParser.js
@@ -0,0 +1,72 @@
+const middy = require('../../middy')
+const urlencodedBodyParser = require('../urlencodeBodyParser')
+
+describe('📦 Middleware URL Encoded Body Parser', () => {
+  test('It should decode simple url encoded requests', () => {
+    const handler = middy((event, context, cb) => {
+      cb(null, event.body) // propagates the body as response
+    })
+
+    handler.use(urlencodedBodyParser({extended: false}))
+
+    // invokes the handler
+    const event = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'frappucino=muffin&goat%5B%5D=scone&pond=moose'
+    }
+
+    handler(event, {}, (_, body) => {
+      expect(body).toEqual({
+        frappucino: 'muffin',
+        'goat[]': 'scone',
+        pond: 'moose'
+      })
+    })
+  })
+
+  test('It should decode complex url encoded requests using extended option', () => {
+    const handler = middy((event, context, cb) => {
+      cb(null, event.body) // propagates the body as response
+    })
+
+    handler.use(urlencodedBodyParser({extended: true}))
+
+    // invokes the handler
+    const event = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'a[b][c][d]=i'
+    }
+
+    handler(event, {}, (_, body) => {
+      expect(body).toEqual({
+        a: {
+          b: {
+            c: {
+              d: 'i'
+            }
+          }
+        }
+      })
+    })
+  })
+
+  test('It shouldn\'t process the body if no header is passed', () => {
+    const handler = middy((event, context, cb) => {
+      cb(null, event.body) // propagates the body as a response
+    })
+
+    handler.use(urlencodedBodyParser())
+
+    // invokes the handler
+    const event = {
+      body: JSON.stringify({foo: 'bar'})
+    }
+    handler(event, {}, (_, body) => {
+      expect(body).toEqual('{"foo":"bar"}')
+    })
+  })
+})
