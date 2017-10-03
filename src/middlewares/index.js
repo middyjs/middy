4a7792640f3e167e61d98a14dda3d65cbef5d412
philipodev <philipw2w@googlemail.com>
2017-10-03 18:21:17 +0200

    exposed cors to middlewares object

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  cors: require('./cors'),
   doNotWaitForEmptyEventLoop: require('./doNotWaitForEmptyEventLoop'),
   httpErrorHandler: require('./httpErrorHandler'),
   jsonBodyParser: require('./jsonBodyParser'),
