2a7d07886efc80ebd16d10b32db8056b56295a35
Jose Santacruz <jose@kushkipagos.com>
2017-11-26 09:10:24 -0500

    First version of typescript type

---
+++ src/middy.d.ts
@@ -0,0 +1,36 @@
+import {Callback, Context, Handler, ProxyResult} from 'aws-lambda';
+
+export interface IMiddy {
+  use: IMiddyUseFunction;
+  before: IMiddyMiddlewareFunction;
+  after: IMiddyMiddlewareFunction;
+  onError: IMiddyMiddlewareFunction;
+  __middlewares: object;
+}
+
+export type IMiddyUseFunction = (config?: object) => IMiddyMiddlewareObject;
+
+interface IMiddyMiddlewareObject {
+  before?: IMiddyMiddlewareFunction;
+  after?: IMiddyMiddlewareFunction;
+  onError?: IMiddyMiddlewareFunction;
+}
+
+type IMiddyMiddlewareFunction = (
+  handler: IHandlerLambda,
+  next: IMiddyNextFunction
+) => void;
+
+export type IMiddyNextFunction = () => void;
+
+export interface IHandlerLambda {
+  event: any;
+  context: Context;
+  response: ProxyResult | object;
+  error: Error;
+  callback: Callback;
+}
+declare let middy: (handler: Handler) => IMiddy;
+
+export default middy;
+
