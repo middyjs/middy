3a0ab2014e8c9b68fe0bd7cfa5798aa55b3f9681
Nick <ng@social-federation.com>
2018-01-12 10:44:33 +0100

    basic tests for lambdaIsWarmingUp middleware

---
+++ src/middlewares/__tests__/lambdaIsWarmingUp.js
@@ -0,0 +1,43 @@
+const middy = require('../../middy')
+const lambdaIsWarmingUp = require('../lambdaIsWarmingUp')
+
+describe('🥃 Do Not Wait For Empty Event Loop', () => {
+  test(`Should exit with 'warmup' if config.isWarmingUp boolean is provided`, () => {
+    const handler = middy((event, context, cb) => {
+      cb()
+    })
+    handler.use(lambdaIsWarmingUp({isWarmingUp: true}))
+
+    const event = {}
+    const context = {}
+    handler(event, context, (_, response) => {
+      expect(response).toBe('warmup')
+    })
+  })
+
+  test(`Should exit with 'warmup' if event.source === 'serverless-warmup-plugin' if no config provided`, () => {
+    const handler = middy((event, context, cb) => {
+      cb()
+    })
+    handler.use(lambdaIsWarmingUp())
+
+    const event = {}
+    const context = {}
+    handler(event, context, (_, response) => {
+      expect(response).toBe('warmup')
+    })
+  })
+
+  test(`Should execute handler if config is empty and event.source !== 'serverless-warmup-plugin'`, () => {
+    const handler = middy((event, context, cb) => {
+      cb(null, 'handler executed')
+    })
+    handler.use(lambdaIsWarmingUp())
+
+    const event = {}
+    const context = {}
+    handler(event, context, (_, response) => {
+      expect(response).toBe('handler executed')
+    })
+  })
+})
