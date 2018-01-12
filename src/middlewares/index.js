cb96e3d1beb6cc5b83fa59e90bab2f47006fbea1
Nick <ng@social-federation.com>
2018-01-12 12:00:55 +0100

    fix isWarmingUp property check + fix middleware/index.js test

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -3,8 +3,8 @@ module.exports = {
   doNotWaitForEmptyEventLoop: require('./doNotWaitForEmptyEventLoop'),
   httpErrorHandler: require('./httpErrorHandler'),
   jsonBodyParser: require('./jsonBodyParser'),
+  lambdaIsWarmingUp: require('./lambdaIsWarmingUp'),
   s3KeyNormalizer: require('./s3KeyNormalizer'),
   urlEncodeBodyParser: require('./urlEncodeBodyParser'),
-  validator: require('./validator'),
-  lambdaIsWarmingUp: require('./lambdaIsWarmingUp')
+  validator: require('./validator')
 }
--- src/middlewares/lambdaIsWarmingUp.js
+++ src/middlewares/lambdaIsWarmingUp.js
@@ -1,6 +1,6 @@
 module.exports = (config) => {
   // get isWarmingUp boolean from config object or use approach with serverless-warmup-plugin library
-  const isWarmingUp = event => config.isWarmingUp || event.source === 'serverless-warmup-plugin'
+  const isWarmingUp = event => (config && config.isWarmingUp) || event.source === 'serverless-warmup-plugin'
 
   return ({
     before: (handler, next) => {
