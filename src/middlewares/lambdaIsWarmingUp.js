42590ca769fa50cfbaee2cf675fad48870cb5702
Nick <ng@social-federation.com>
2018-01-12 10:55:04 +0100

    Fix PR according to eslint rules

--- src/middlewares/lambdaIsWarmingUp.js
+++ src/middlewares/lambdaIsWarmingUp.js
@@ -1,15 +1,15 @@
 module.exports = (config) => {
-    // get isWarmingUp boolean from config object or use approach with serverless-warmup-plugin library
-    const isWarmingUp = event => config.isWarmingUp || event.source === 'serverless-warmup-plugin'
+  // get isWarmingUp boolean from config object or use approach with serverless-warmup-plugin library
+  const isWarmingUp = event => config.isWarmingUp || event.source === 'serverless-warmup-plugin'
 
-    return ({
-      before: (handler, next) => {
-        if (isWarmingUp(event)) {
-            console.log('Exiting early via warmUpMiddleware')
-            return handler.callback(null, "warmup") //not provide any specific information here since it will not be shown
-        }
-
-        next()
+  return ({
+    before: (handler, next) => {
+      if (isWarmingUp(handler.event)) {
+        console.log('Exiting early via warmUpMiddleware')
+        return handler.callback(null, 'warmup')// not provide any specific information here since it will not be shown
       }
-    })
-  }
+
+      next()
+    }
+  })
+}
