63ad25127dd35f040b106267c6531142c039eb8a
Joël Galeran <Jolg42@users.noreply.github.com>
2018-01-12 17:34:06 +0100

    Fix urlEncodeBodyParser when Content-Type header contains charset
    Decode body when Content-Type contains more than 'application/x-www-form-urlencoded'.
    Example: 'application/x-www-form-urlencoded; charset=UTF-8'

--- src/middlewares/urlEncodeBodyParser.js
+++ src/middlewares/urlEncodeBodyParser.js
@@ -8,9 +8,10 @@ module.exports = (opts) => ({
 
     const parserFn = options.extended ? require('qs').parse : require('querystring').decode
 
-    if (handler.event.headers && handler.event.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+    if (handler.event.headers && handler.event.headers['Content-Type'].indexOf('application/x-www-form-urlencoded') === 0) {
       handler.event.body = parserFn(handler.event.body)
     }
+    
     next()
   }
 })
