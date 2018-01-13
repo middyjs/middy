01d9a273f37e8d65eab8d60cd856ebdfdd8b326c
Luciano Mammino <lucianomammino@gmail.com>
2018-01-13 10:36:12 -0600

    Little improvements and standardisation

--- src/middlewares/__tests__/warmup.js
+++ src/middlewares/__tests__/warmup.js
@@ -1,25 +1,37 @@
 const middy = require('../../middy')
 const lambdaIsWarmingUp = require('../warmup')
 
+console.log = jest.fn()
+
+beforeEach(() => {
+  console.log.mockClear()
+})
+
 describe('🥃 Warmup', () => {
-  test(`Should exit with 'warmup' if provided warmup check function is provide and returns false`, () => {
+  test(`Should exit with 'warmup' if provided warmup check function is provide and returns true`, (endTest) => {
     const handler = middy((event, context, cb) => {
       cb()
     })
-    handler.use(lambdaIsWarmingUp({isWarmingUp: (event) => true}))
+    handler.use(lambdaIsWarmingUp({
+      isWarmingUp: (event) => true,
+      onWarmup: () => {}
+    }))
 
     const event = {}
     const context = {}
     handler(event, context, (_, response) => {
       expect(response).toBe('warmup')
+      endTest()
     })
   })
 
-  test(`Should exit with 'warmup' if event.source === 'serverless-warmup-plugin' if no warmup check function provided`, () => {
+  test(`Should exit with 'warmup' if event.source === 'serverless-warmup-plugin' if no warmup check function provided`, (endTest) => {
     const handler = middy((event, context, cb) => {
       cb()
     })
-    handler.use(lambdaIsWarmingUp())
+    handler.use(lambdaIsWarmingUp({
+      onWarmup: () => {}
+    }))
 
     const event = {
       source: 'serverless-warmup-plugin'
@@ -27,10 +39,30 @@ describe('🥃 Warmup', () => {
     const context = {}
     handler(event, context, (_, response) => {
       expect(response).toBe('warmup')
+      endTest()
     })
   })
 
-  test(`Should execute handler if provided warmup check function returns false`, () => {
+  test(`It should print in the console when exiting because of warmup and the onWarmup function is not redefined`, (endTest) => {
+    console.log = jest.fn()
+
+    const handler = middy((event, context, cb) => {
+      cb()
+    })
+    handler.use(lambdaIsWarmingUp({}))
+
+    const event = {
+      source: 'serverless-warmup-plugin'
+    }
+    const context = {}
+    handler(event, context, (_, response) => {
+      expect(response).toBe('warmup')
+      expect(console.log).toHaveBeenCalled()
+      endTest()
+    })
+  })
+
+  test(`Should execute handler if provided warmup check function returns false`, (endTest) => {
     const handler = middy((event, context, cb) => {
       cb(null, 'handler executed')
     })
@@ -40,18 +72,7 @@ describe('🥃 Warmup', () => {
     const context = {}
     handler(event, context, (_, response) => {
       expect(response).toBe('handler executed')
+      endTest()
     })
-  })
-
-  test(`Should throw error if boolean passed as warmup check function`, () => {
-    const handler = middy((event, context, cb) => {
-      cb()
-    })
-
-    try {
-      handler.use(lambdaIsWarmingUp({isWarmingUp: true}))
-    } catch (ex) {
-      expect(ex.message).toBe('config.isWarmingUp should be a function')
-    }
   })
 })
--- src/middlewares/warmup.js
+++ src/middlewares/warmup.js
@@ -1,19 +1,16 @@
 module.exports = (config) => {
   const defaults = {
-    isWarmingUp: (event) => event.source === 'serverless-warmup-plugin'
+    isWarmingUp: (event) => event.source === 'serverless-warmup-plugin',
+    onWarmup: (event) => console.log('Exiting early via warmup Middleware')
   }
 
   const options = Object.assign({}, defaults, config)
 
-  if (!(options.isWarmingUp instanceof Function)) {
-    throw Error('config.isWarmingUp should be a function')
-  }
-
   return ({
     before: (handler, next) => {
       if (options.isWarmingUp(handler.event)) {
-        console.log('Exiting early via warmUpMiddleware')
-        return handler.callback(null, 'warmup')// not provide any specific information here since it will not be shown
+        options.onWarmup(handler.event)
+        return handler.callback(null, 'warmup')
       }
 
       next()
