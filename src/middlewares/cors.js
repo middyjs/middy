b8eb4c7d2612561f8ee70e06f9e6db17a6216d50
Luciano Mammino <lmammino@vectra.ai>
2018-01-28 12:07:24 +0000

    reverted changes in cors middleware

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -8,6 +8,7 @@ function addCorsHeaders (opts, handler, next) {
   if (handler.event.hasOwnProperty('httpMethod')) {
     handler.response = handler.response || {}
     handler.response.headers = handler.response.headers || {}
+
     if (!handler.response.headers.hasOwnProperty('Access-Control-Allow-Origin')) {
       handler.response.headers['Access-Control-Allow-Origin'] = options.origin
     }
