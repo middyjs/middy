9dc1b82bab5dbdfdde8790d5f61161f375fc0a8d
Vlad Holubiev <golubev.vld@gmail.com>
2018-02-06 19:56:09 +0200

    feat: allow passing custom options to ajv constructor in validator m-are

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,38 +1,79 @@
 const createError = require('http-errors')
 const Ajv = require('ajv')
 const ajvKeywords = require('ajv-keywords')
+const {deepEqual} = require('assert')
 
-const ajv = new Ajv({v5: true, $data: true, allErrors: true})
-ajvKeywords(ajv)
+let ajv
+let previousConstructorOptions
+const defaults = {v5: true, $data: true, allErrors: true}
 
-module.exports = ({inputSchema, outputSchema}, options) => {
+module.exports = ({inputSchema, outputSchema, ajvOptions}) => {
+  const options = Object.assign({}, defaults, ajvOptions)
+  lazyLoadAjv(options)
+
   const validateInput = inputSchema ? ajv.compile(inputSchema) : null
   const validateOutput = outputSchema ? ajv.compile(outputSchema) : null
+
   return {
-    before: (handler, next) => {
+    before (handler, next) {
       if (!inputSchema) {
         return next()
       }
+
       const valid = validateInput(handler.event)
+
       if (!valid) {
         const error = new createError.BadRequest('Event object failed validation')
         error.details = validateInput.errors
         throw error
       }
+
       return next()
     },
-    after: (handler, next) => {
+    after (handler, next) {
       if (!outputSchema) {
-        return next()
+        return next(null, 'from after')
       }
+
       const valid = validateOutput(handler.response)
+
       if (!valid) {
         const error = new createError.InternalServerError('Response object failed validation')
         error.details = validateOutput.errors
         error.response = handler.response
         throw error
       }
+
       return next()
     }
   }
+}
+
+function lazyLoadAjv (options) {
+  if (shouldInitAjv(options)) {
+    initAjv(options)
+  }
+
+  return ajv
+}
+
+function shouldInitAjv (options) {
+  return !ajv || areConstructorOptionsNew(options)
+}
+
+function areConstructorOptionsNew (options) {
+  try {
+    deepEqual(options, previousConstructorOptions)
+  } catch (e) {
+    return true
+  }
+
+  return false
+}
+
+function initAjv (options) {
+  ajv = new Ajv(options)
+  ajvKeywords(ajv)
+
+  previousConstructorOptions = options
 }
