57925784b2c54e4f4b1a9d9eacc836e5617ea3fd
Sebastian Domagała <sdomagala@users.noreply.github.com>
2018-09-20 21:57:10 +0200

    update secrets manager with throwOnFailedCall flag (#236)
    * Initial work for 1.0.0 with secrets-manager
    
    * bump typings
    
    * bump package-locks
    
    * bump version to 17
    
    * fix package locks

--- packages/secrets-manager/index.js
+++ packages/secrets-manager/index.js
@@ -4,6 +4,7 @@ module.exports = opts => {
   const defaults = {
     awsSdkOptions: {},
     secrets: {}, // e.g. { RDS_SECRET: 'dev/rds_login', API_SECRET: '...' }
+    throwOnFailedCall: false,
     cache: false,
     cacheExpiryInMillis: undefined,
     secretsLoaded: false,
@@ -54,6 +55,10 @@ module.exports = opts => {
         })
         .catch(err => {
           console.error('failed to refresh secrets from Secrets Manager:', err.message)
+          // throw error if there is no secret in cache already and flag throwOnFailedCall provided
+          if (options.throwOnFailedCall && !options.secretsCache) {
+            throw err
+          }
           // if we already have a cached secrets, then reset the timestamp so we don't
           // keep retrying on every invocation which can cause performance problems
           // when there's temporary problems with Secrets Manager
