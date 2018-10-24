efc3b8166ccbf6542bd0c209a057365297eb0e54
Yuri Shapira <yuri@puresec.io>
2018-10-24 15:22:07 +0300

    initialize functionShield only on coldstart

--- src/middlewares/functionShield.js
+++ src/middlewares/functionShield.js
@@ -1,21 +1,24 @@
-const functionShield = require('@puresec/function-shield')
-
+let coldStart = true
 module.exports = (opts = {policy: {}}) => ({
   before: (handler, next) => {
-    const defaults = {
-      policy: {
-        outbound_connectivity: 'block',
-        read_write_tmp: 'block',
-        create_child_process: 'block',
-        read_handler: 'block'
-      },
-      disable_analytics: false,
-      token: process.env.FUNCTION_SHIELD_TOKEN || handler.context.FUNCTION_SHIELD_TOKEN
-    }
-    let options = Object.assign({}, defaults, opts)
-    options.policy = Object.assign({}, defaults.policy, opts.policy)
+    if (coldStart) {
+      const functionShield = require('@puresec/function-shield')
+      const defaults = {
+        policy: {
+          outbound_connectivity: 'block',
+          read_write_tmp: 'block',
+          create_child_process: 'block',
+          read_handler: 'block'
+        },
+        disable_analytics: false,
+        token: process.env.FUNCTION_SHIELD_TOKEN || handler.context.FUNCTION_SHIELD_TOKEN
+      }
+      let options = Object.assign({}, defaults, opts)
+      options.policy = Object.assign({}, defaults.policy, opts.policy)
 
-    functionShield.configure(options)
+      functionShield.configure(options)
+      coldStart = false
+    }
 
     next()
   }
