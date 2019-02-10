60d4cbb1f0583e58fa400ac212e78ffee55ba263
Luciano <lucianomammino@gmail.com>
2019-02-10 18:42:19 +0000

    Updated typings and added a CHANGELOG

--- packages/core/index.d.ts
+++ packages/core/index.d.ts
@@ -1,40 +1,45 @@
-import { Callback, Context, Handler, ProxyResult } from 'aws-lambda';
+import { Callback, Context, Handler, ProxyResult } from 'aws-lambda'
 
-type AsyncHandler = (event: any, context: Context) => Promise<ProxyResult | object>;
+type AsyncHandler = (
+  event: any,
+  context: Context
+) => Promise<ProxyResult | object>
 
 declare var middy: {
-  (handler: Handler | AsyncHandler): middy.IMiddy;
-};
+(handler: Handler | AsyncHandler): middy.IMiddy
+}
 
 declare namespace middy {
   interface IMiddy extends Handler {
-    use: IMiddyUseFunction;
-    before: IMiddyMiddlewareFunction;
-    after: IMiddyMiddlewareFunction;
-    onError: IMiddyMiddlewareFunction;
+    use: IMiddyUseFunction
+    before: IMiddyMiddlewareFunction
+    after: IMiddyMiddlewareFunction
+    onError: IMiddyMiddlewareFunction
   }
 
-  type IMiddyUseFunction = (config?: object) => IMiddy;
+  type IMiddyUseFunction = (
+    middlewares: IMiddyMiddlewareObject | IMiddyMiddlewareObject[]
+  ) => IMiddy
 
   interface IMiddyMiddlewareObject {
-    before?: IMiddyMiddlewareFunction;
-    after?: IMiddyMiddlewareFunction;
-    onError?: IMiddyMiddlewareFunction;
+    before?: IMiddyMiddlewareFunction
+    after?: IMiddyMiddlewareFunction
+    onError?: IMiddyMiddlewareFunction
   }
 
   type IMiddyMiddlewareFunction = (
     handler: IHandlerLambda,
     next: IMiddyNextFunction
-  ) => void | Promise<any>;
+  ) => void | Promise<any>
 
-  type IMiddyNextFunction = (error?: any) => void;
+  type IMiddyNextFunction = (error?: any) => void
 
   interface IHandlerLambda<T = any, V = object> {
-    event: T;
-    context: Context;
-    response: V;
-    error: Error;
-    callback: Callback;
+    event: T
+    context: Context
+    response: V
+    error: Error
+    callback: Callback
   }
 }
 
