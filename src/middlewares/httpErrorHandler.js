0c66015835503e6cff20732da58d3d106aef6f06
Gurarpit Singh <gurarpit.singh@dazn.com>
2019-03-26 18:04:37 +0000

    Revert "prevent httpErrorHandler from swallowing non http errors when used with cors"
    This reverts commit fa7f66db071869a897f76067a6923e741bf836dd.

--- src/middlewares/httpErrorHandler.js
+++ src/middlewares/httpErrorHandler.js
@@ -7,21 +7,17 @@ module.exports = (opts) => {
 
   return ({
     onError: (handler, next) => {
-      if (typeof options.logger === 'function') {
-        options.logger(handler.error)
-      }
-
-      handler.response = handler.response || {}
       if (handler.error.constructor.super_ && handler.error.constructor.super_.name === 'HttpError') {
+        if (typeof options.logger === 'function') {
+          options.logger(handler.error)
+        }
+
         handler.response = {
           statusCode: handler.error.statusCode,
           body: handler.error.message
         }
 
         return next()
-      } else {
-        handler.response.statusCode = 502
-        handler.response.body = JSON.stringify({ message: 'Internal server error' })
       }
 
       return next(handler.error)
