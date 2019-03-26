fa7f66db071869a897f76067a6923e741bf836dd
Gurarpit Singh <gurarpit.singh@dazn.com>
2019-03-26 16:55:56 +0000

    prevent httpErrorHandler from swallowing non http errors when used with cors

--- src/middlewares/httpErrorHandler.js
+++ src/middlewares/httpErrorHandler.js
@@ -7,17 +7,21 @@ module.exports = (opts) => {
 
   return ({
     onError: (handler, next) => {
-      if (handler.error.constructor.super_ && handler.error.constructor.super_.name === 'HttpError') {
-        if (typeof options.logger === 'function') {
-          options.logger(handler.error)
-        }
+      if (typeof options.logger === 'function') {
+        options.logger(handler.error)
+      }
 
+      handler.response = handler.response || {}
+      if (handler.error.constructor.super_ && handler.error.constructor.super_.name === 'HttpError') {
         handler.response = {
           statusCode: handler.error.statusCode,
           body: handler.error.message
         }
 
         return next()
+      } else {
+        handler.response.statusCode = 502
+        handler.response.body = JSON.stringify({ message: 'Internal server error' })
       }
 
       return next(handler.error)
