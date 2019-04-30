68720fc1b79087cfdd273641fddb852b5de28df6
Roni Frantchi <roni.frantchi@gmail.com>
2019-04-30 19:32:33 +0300

    add reviver support for http-json-body-parser

--- packages/http-json-body-parser/index.js
+++ packages/http-json-body-parser/index.js
@@ -1,7 +1,7 @@
 const createError = require('http-errors')
 const contentType = require('content-type')
 
-module.exports = () => ({
+module.exports = reviver => ({
   before: (handler, next) => {
     if (handler.event.headers) {
       const contentTypeHeader = handler.event.headers['content-type'] || handler.event.headers['Content-Type']
@@ -9,7 +9,7 @@ module.exports = () => ({
         const { type } = contentType.parse(contentTypeHeader)
         if (type === 'application/json') {
           try {
-            handler.event.body = JSON.parse(handler.event.body)
+            handler.event.body = JSON.parse(handler.event.body, reviver)
           } catch (err) {
             throw new createError.UnprocessableEntity('Content type defined as JSON but an invalid JSON was provided')
           }
