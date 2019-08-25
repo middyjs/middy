171db61f8c019c4e16e53ba9f6ebefd18ad08cdb
Chris Shepherd <chris@chrisshepherd.me>
2019-08-25 19:42:18 +0100

    Expose types in core typings

--- packages/core/index.d.ts
+++ packages/core/index.d.ts
@@ -1,46 +1,42 @@
 import { Callback, Context, Handler, ProxyResult } from 'aws-lambda'
 
-type AsyncHandler = (
+export type AsyncHandler = (
   event: any,
   context: Context
 ) => Promise<ProxyResult | object>
 
-declare var middy: {
-(handler: Handler | AsyncHandler): middy.IMiddy
+declare const middy: (handler: Handler | AsyncHandler) => IMiddy
+
+export interface IMiddy extends Handler {
+  use: IMiddyUseFunction
+  before: IMiddyMiddlewareFunction
+  after: IMiddyMiddlewareFunction
+  onError: IMiddyMiddlewareFunction
 }
 
-declare namespace middy {
-  interface IMiddy extends Handler {
-    use: IMiddyUseFunction
-    before: IMiddyMiddlewareFunction
-    after: IMiddyMiddlewareFunction
-    onError: IMiddyMiddlewareFunction
-  }
+export type IMiddyUseFunction = (
+  middlewares?: IMiddyMiddlewareObject | IMiddyMiddlewareObject[]
+) => IMiddy
 
-  type IMiddyUseFunction = (
-    middlewares?: IMiddyMiddlewareObject | IMiddyMiddlewareObject[]
-  ) => IMiddy
+export interface IMiddyMiddlewareObject {
+  before?: IMiddyMiddlewareFunction
+  after?: IMiddyMiddlewareFunction
+  onError?: IMiddyMiddlewareFunction
+}
 
-  interface IMiddyMiddlewareObject {
-    before?: IMiddyMiddlewareFunction
-    after?: IMiddyMiddlewareFunction
-    onError?: IMiddyMiddlewareFunction
-  }
+export type IMiddyMiddlewareFunction = (
+  handler: IHandlerLambda,
+  next: IMiddyNextFunction
+) => void | Promise<any>
 
-  type IMiddyMiddlewareFunction = (
-    handler: IHandlerLambda,
-    next: IMiddyNextFunction
-  ) => void | Promise<any>
+export type IMiddyNextFunction = (error?: any) => void
 
-  type IMiddyNextFunction = (error?: any) => void
-
-  interface IHandlerLambda<T = any, V = object> {
-    event: T
-    context: Context
-    response: V
-    error: Error
-    callback: Callback
-  }
+export interface IHandlerLambda<T = any, V = object> {
+  event: T
+  context: Context
+  response: V
+  error: Error
+  callback: Callback
 }
 
 export default middy
