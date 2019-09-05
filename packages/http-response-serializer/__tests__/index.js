7e90153276d83891d99636c4a38579a7afdfcce0
Vlad Holubiev <golubev.vld@gmail.com>
2019-09-05 21:33:16 +0300

    test: rewrite "packages/http-response-serializer" tests to async/await
    fixes #270

--- packages/http-response-serializer/__tests__/index.js
+++ packages/http-response-serializer/__tests__/index.js
@@ -1,4 +1,5 @@
 const createError = require('http-errors')
+const { invoke } = require('../../test-helpers')
 const middy = require('../../core')
 const httpErrorHandler = require('../../http-error-handler')
 const httpResponseSerializer = require('../')
@@ -27,14 +28,14 @@ const createHttpResponse = () => ({
 })
 
 describe('📦  Middleware Http Response Serializer', () => {
-  describe('It should pass-through when `content-type` header is already set', () => {
+  describe('It should pass-through when `content-type` header is already set', async () => {
     test.each([
       ['Content-Type'],
       ['content-type'],
       ['CONTENT-TYPE']
     ])(
       '%s skips response serialization',
-      (key, done) => {
+      async (key) => {
         const handlerResponse = Object.assign({}, createHttpResponse(), {
           headers: {
             [key]: 'text/plain'
@@ -44,21 +45,20 @@ describe('📦  Middleware Http Response Serializer', () => {
 
         handler.use(httpResponseSerializer(standardConfiguration))
 
-        handler({}, {}, (_, response) => {
-          expect(response).toEqual(handlerResponse)
-          done()
-        })
+        const response = await invoke(handler)
+
+        expect(response).toEqual(handlerResponse)
       })
   })
 
-  describe('It should find the correct serializer from the request accept header', () => {
+  describe('It should find the correct serializer from the request accept header', async () => {
     test.each([
       ['application/xml, text/x-dvi; q=0.8, text/x-c', '<message>Hello World</message>'],
       ['application/json, text/plain, */*', '{"message":"Hello World"}'],
       ['text/plain, text/x-c', 'Hello World']
     ])(
       '%s returns %s',
-      (accept, result, done) => {
+      async (accept, result) => {
         const handler = middy((event, context, cb) => cb(null, createHttpResponse()))
 
         handler.use(httpResponseSerializer(standardConfiguration))
@@ -69,14 +69,13 @@ describe('📦  Middleware Http Response Serializer', () => {
           }
         }
 
-        handler(event, {}, (_, response) => {
-          expect(response.body).toEqual(result)
-          done()
-        })
+        const response = await invoke(handler, event)
+
+        expect(response.body).toEqual(result)
       })
   })
 
-  test('It should use `event.requiredContentType` instead of accept headers', done => {
+  test('It should use `event.requiredContentType` instead of accept headers', async () => {
     const handler = middy((event, context, cb) => {
       event.requiredContentType = 'text/plain'
 
@@ -91,38 +90,36 @@ describe('📦  Middleware Http Response Serializer', () => {
       }
     }
 
-    handler(event, {}, (_, response) => {
-      expect(response).toEqual({
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'text/plain'
-        },
-        body: 'Hello World'
-      })
-      done()
+    const response = await invoke(handler, event)
+
+    expect(response).toEqual({
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      body: 'Hello World'
     })
   })
 
-  test('It should use the default when no accept preferences are given', done => {
+  test('It should use the default when no accept preferences are given', async () => {
     const handler = middy((event, context, cb) =>
       cb(null, createHttpResponse())
     )
 
     handler.use(httpResponseSerializer(standardConfiguration))
 
-    handler({}, {}, (_, response) => {
-      expect(response).toEqual({
-        statusCode: 200,
-        headers: {
-          'Content-Type': standardConfiguration.default
-        },
-        body: '{"message":"Hello World"}'
-      })
-      done()
+    const response = await invoke(handler)
+
+    expect(response).toEqual({
+      statusCode: 200,
+      headers: {
+        'Content-Type': standardConfiguration.default
+      },
+      body: '{"message":"Hello World"}'
     })
   })
 
-  test('It should use `event.preferredContentType` instead of the default', done => {
+  test('It should use `event.preferredContentType` instead of the default', async () => {
     const handler = middy((event, context, cb) => {
       event.preferredContentType = 'text/plain'
 
@@ -131,19 +128,18 @@ describe('📦  Middleware Http Response Serializer', () => {
 
     handler.use(httpResponseSerializer(standardConfiguration))
 
-    handler({}, {}, (_, response) => {
-      expect(response).toEqual({
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'text/plain'
-        },
-        body: 'Hello World'
-      })
-      done()
+    const response = await invoke(handler)
+
+    expect(response).toEqual({
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      body: 'Hello World'
     })
   })
 
-  test('It should pass-through when no preference or default is found', done => {
+  test('It should pass-through when no preference or default is found', async () => {
     const handler = middy((event, context, cb) =>
       cb(null, createHttpResponse())
     )
@@ -152,16 +148,15 @@ describe('📦  Middleware Http Response Serializer', () => {
       serializers: standardConfiguration.serializers
     }))
 
-    handler({}, {}, (_, response) => {
-      expect(response).toEqual({
-        statusCode: 200,
-        body: 'Hello World'
-      })
-      done()
+    const response = await invoke(handler)
+
+    expect(response).toEqual({
+      statusCode: 200,
+      body: 'Hello World'
     })
   })
 
-  test('It should not pass-through when request content-type is set', done => {
+  test('It should not pass-through when request content-type is set', async () => {
     const handler = middy((event, context, cb) =>
       cb(null, createHttpResponse())
     )
@@ -174,20 +169,18 @@ describe('📦  Middleware Http Response Serializer', () => {
       }
     }
 
-    handler(event, {}, (err, response) => {
-      if (err) throw err
-      expect(response).toEqual({
-        statusCode: 200,
-        headers: {
-          'Content-Type': standardConfiguration.default
-        },
-        body: '{"message":"Hello World"}'
-      })
-      done()
+    const response = await invoke(handler, event)
+
+    expect(response).toEqual({
+      statusCode: 200,
+      headers: {
+        'Content-Type': standardConfiguration.default
+      },
+      body: '{"message":"Hello World"}'
     })
   })
 
-  test('It should replace the response object when the serializer returns an object', done => {
+  test('It should replace the response object when the serializer returns an object', async () => {
     const handler = middy((event, context, cb) =>
       cb(null, createHttpResponse())
     )
@@ -205,20 +198,19 @@ describe('📦  Middleware Http Response Serializer', () => {
       default: 'text/plain'
     }))
 
-    handler({}, {}, (_, response) => {
-      expect(response).toEqual({
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'text/plain'
-        },
-        body: 'SGVsbG8gV29ybGQ=',
-        isBase64Encoded: true
-      })
-      done()
+    const response = await invoke(handler)
+
+    expect(response).toEqual({
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      body: 'SGVsbG8gV29ybGQ=',
+      isBase64Encoded: true
     })
   })
 
-  test('It should work with `http-error-handler` middleware', done => {
+  test('It should work with `http-error-handler` middleware', async () => {
     const handler = middy((event, context, cb) => {
       throw new createError.UnprocessableEntity()
     })
@@ -227,19 +219,18 @@ describe('📦  Middleware Http Response Serializer', () => {
       .use(httpErrorHandler())
       .use(httpResponseSerializer(standardConfiguration))
 
-    handler({}, {}, (_, response) => {
-      expect(response).toEqual({
-        statusCode: 422,
-        body: '{"message":"Unprocessable Entity"}',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      done()
+    const response = await invoke(handler)
+
+    expect(response).toEqual({
+      statusCode: 422,
+      body: '{"message":"Unprocessable Entity"}',
+      headers: {
+        'Content-Type': 'application/json'
+      }
     })
   })
 
-  test('It should not crash if the response is undefined', done => {
+  test('It should not crash if the response is undefined', async () => {
     const handler = middy((event, context, cb) =>
       cb(null, undefined)
     )
@@ -252,15 +243,13 @@ describe('📦  Middleware Http Response Serializer', () => {
       }
     }
 
-    handler(event, {}, (err, response) => {
-      if (err) throw err
-      expect(response).toEqual({
-        headers: {
-          'Content-Type': standardConfiguration.default
-        },
-        body: '{}'
-      })
-      done()
+    const response = await invoke(handler, event)
+
+    expect(response).toEqual({
+      headers: {
+        'Content-Type': standardConfiguration.default
+      },
+      body: '{}'
     })
   })
 })
