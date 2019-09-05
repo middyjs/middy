ca77801f44941f6f49612421c9a26356bbdf834a
Vlad Holubiev <golubev.vld@gmail.com>
2019-09-05 21:19:38 +0300

    test: rewrite "packages/http-error-handler" tests to async/await
    fixes #270

--- packages/http-error-handler/__tests__/index.js
+++ packages/http-error-handler/__tests__/index.js
@@ -1,59 +1,60 @@
 const createError = require('http-errors')
+const { invoke } = require('../../test-helpers')
 const middy = require('../../core')
 const httpErrorHandler = require('../')
 
-describe('📦 Middleware Http Error Handler', () => {
-  test('It should create a response for HTTP errors', () => {
-    const handler = middy((event, context, cb) => {
+describe('📦 Middleware Http Error Handler', async () => {
+  test('It should create a response for HTTP errors', async () => {
+    const handler = middy(() => {
       throw new createError.UnprocessableEntity()
     })
 
     handler
       .use(httpErrorHandler({ logger: false }))
 
-    // run the handler
-    handler({}, {}, (_, response) => {
-      expect(response).toEqual({
-        statusCode: 422,
-        body: 'Unprocessable Entity'
-      })
+    const response = await invoke(handler)
+
+    expect(response).toEqual({
+      statusCode: 422,
+      body: 'Unprocessable Entity'
     })
   })
 
-  test('It should NOT handle non HTTP errors', () => {
-    const handler = middy((event, context, cb) => {
+  test('It should NOT handle non HTTP errors', async () => {
+    expect.assertions(1)
+
+    const handler = middy(() => {
       throw new Error('non-http error')
     })
 
     handler
       .use(httpErrorHandler({ logger: false }))
 
-    // run the handler
-    handler({}, {}, (error, response) => {
-      expect(response).toBe(undefined)
+    try {
+      await invoke(handler)
+    } catch (error) {
       expect(error.message).toEqual('non-http error')
-    })
+    }
   })
 
-  test('It should be possible to pass a custom logger function', () => {
+  test('It should be possible to pass a custom logger function', async () => {
     const expectedError = new createError.UnprocessableEntity()
     const logger = jest.fn()
 
-    const handler = middy((event, context, cb) => {
+    const handler = middy(() => {
       throw expectedError
     })
 
     handler
       .use(httpErrorHandler({ logger }))
 
-    // run the handler
-    handler({}, {}, (_, response) => {
-      expect(logger).toHaveBeenCalledWith(expectedError)
-    })
+    await invoke(handler)
+
+    expect(logger).toHaveBeenCalledWith(expectedError)
   })
 
-  test('It should create a response for HTTP errors created with a generic error', () => {
-    const handler = middy((event, context, cb) => {
+  test('It should create a response for HTTP errors created with a generic error', async () => {
+    const handler = middy(() => {
       const err = new Error('A server error')
       err.statusCode = 500
       throw err
@@ -62,12 +63,11 @@ describe('📦 Middleware Http Error Handler', () => {
     handler
       .use(httpErrorHandler({ logger: false }))
 
-    // run the handler
-    handler({}, {}, (_, response) => {
-      expect(response).toEqual({
-        statusCode: 500,
-        body: 'A server error'
-      })
+    const response = await invoke(handler)
+
+    expect(response).toEqual({
+      statusCode: 500,
+      body: 'A server error'
     })
   })
 })
