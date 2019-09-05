65013ee2263c96802252be943991f7b14fb8621f
Vlad Holubiev <golubev.vld@gmail.com>
2019-09-05 21:25:14 +0300

    test: rewrite "packages/http-header-normalizer" tests to async/await
    fixes #270

--- packages/http-header-normalizer/__tests__/index.js
+++ packages/http-header-normalizer/__tests__/index.js
@@ -1,8 +1,9 @@
+const { invoke } = require('../../test-helpers')
 const middy = require('../../core')
 const httpHeaderNormalizer = require('../')
 
 describe('👺 Middleware Http Header Normalizer', () => {
-  test('It should normalize (lowercase) all the headers and create a copy in rawHeaders', (endTest) => {
+  test('It should normalize (lowercase) all the headers and create a copy in rawHeaders', async () => {
     const handler = middy((event, context, cb) => cb(null, event))
 
     handler
@@ -28,15 +29,13 @@ describe('👺 Middleware Http Header Normalizer', () => {
 
     const originalHeaders = Object.assign({}, event.headers)
 
-    // run the handler
-    handler(event, {}, (_, resultingEvent) => {
-      expect(resultingEvent.headers).toEqual(expectedHeaders)
-      expect(resultingEvent.rawHeaders).toEqual(originalHeaders)
-      endTest()
-    })
+    const resultingEvent = await invoke(handler, event)
+
+    expect(resultingEvent.headers).toEqual(expectedHeaders)
+    expect(resultingEvent.rawHeaders).toEqual(originalHeaders)
   })
 
-  test('It should normalize (canonical) all the headers and create a copy in rawHeaders', (endTest) => {
+  test('It should normalize (canonical) all the headers and create a copy in rawHeaders', async () => {
     const handler = middy((event, context, cb) => cb(null, event))
 
     handler
@@ -62,15 +61,13 @@ describe('👺 Middleware Http Header Normalizer', () => {
 
     const originalHeaders = Object.assign({}, event.headers)
 
-    // run the handler
-    handler(event, {}, (_, resultingEvent) => {
-      expect(resultingEvent.headers).toEqual(expectedHeaders)
-      expect(resultingEvent.rawHeaders).toEqual(originalHeaders)
-      endTest()
-    })
+    const resultingEvent = await invoke(handler, event)
+
+    expect(resultingEvent.headers).toEqual(expectedHeaders)
+    expect(resultingEvent.rawHeaders).toEqual(originalHeaders)
   })
 
-  test('It can use custom normalization function', (endTest) => {
+  test('It can use custom normalization function', async () => {
     const normalizeHeaderKey = (key) => key.toUpperCase()
 
     const handler = middy((event, context, cb) => cb(null, event))
@@ -100,15 +97,13 @@ describe('👺 Middleware Http Header Normalizer', () => {
 
     const originalHeaders = Object.assign({}, event.headers)
 
-    // run the handler
-    handler(event, {}, (_, resultingEvent) => {
-      expect(resultingEvent.headers).toEqual(expectedHeaders)
-      expect(resultingEvent.rawHeaders).toEqual(originalHeaders)
-      endTest()
-    })
+    const resultingEvent = await invoke(handler, event)
+
+    expect(resultingEvent.headers).toEqual(expectedHeaders)
+    expect(resultingEvent.rawHeaders).toEqual(originalHeaders)
   })
 
-  test('It should not fail if the event does not contain headers', (endTest) => {
+  test('It should not fail if the event does not contain headers', async () => {
     const handler = middy((event, context, cb) => cb(null, event))
 
     handler
@@ -122,11 +117,9 @@ describe('👺 Middleware Http Header Normalizer', () => {
       foo: 'bar'
     }
 
-    // run the handler
-    handler(event, {}, (_, resultingEvent) => {
-      expect(resultingEvent).toEqual(expectedEvent)
-      expect(resultingEvent.rawHeaders).toBeUndefined()
-      endTest()
-    })
+    const resultingEvent = await invoke(handler, event)
+
+    expect(resultingEvent).toEqual(expectedEvent)
+    expect(resultingEvent.rawHeaders).toBeUndefined()
   })
 })
