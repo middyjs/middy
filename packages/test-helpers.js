63f8585845a2ee126fb28f4e49dfeacea92d2d6d
Luciano <lucianomammino@gmail.com>
2019-09-15 18:03:17 +0100

    importing es6-promisify correctly

--- packages/test-helpers.js
+++ packages/test-helpers.js
@@ -1,7 +1,4 @@
-let { promisify } = require('util')
-if (typeof promisify === 'undefined') {
-  promisify = require('es6-promisify')
-}
+const { promisify } = require('util') || require('es6-promisify')
 
 function invoke (handler, event = {}, context = {}) {
   return promisify(handler)(event, context)
