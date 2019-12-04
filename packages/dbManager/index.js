4d1f2983b3c8f86bee6c87bff5cafe6d27963746
Sebastian Domagała <sdomagala@users.noreply.github.com>
2019-12-04 10:42:31 +0100

    dbManager middleware @ monorepo (#407)
    * dbManager middleware @ monorepo
    
    * just some whitespaces
    
    * downgrade knex for node@6 support
    
    * remove typescript from db manager
    
    * remove redundant ignores
    
    * version bump

---
+++ packages/dbManager/index.js
@@ -0,0 +1,53 @@
+const knex = require('knex')
+
+let dbInstance
+
+module.exports = (opts) => {
+  const defaults = {
+    client: knex,
+    config: null,
+    forceNewConnection: false,
+    secretsPath: null, // provide path where credentials lay in context
+    removeSecrets: true
+  }
+
+  const options = Object.assign({}, defaults, opts)
+
+  function cleanup (handler, next) {
+    if (options.forceNewConnection && (dbInstance && typeof dbInstance.destroy === 'function')) {
+      dbInstance.destroy((err) => next(err || handler.error))
+    }
+    next(handler.error)
+  }
+
+  return {
+    before: (handler, next) => {
+      const {
+        client,
+        config,
+        forceNewConnection,
+        secretsPath,
+        removeSecrets
+      } = options
+
+      if (!config) {
+        throw new Error('Config is required in dbManager')
+      }
+      if (!dbInstance || forceNewConnection) {
+        if (secretsPath) {
+          config.connection = Object.assign({}, config.connection || {}, handler.context[secretsPath])
+        }
+        dbInstance = client(config)
+      }
+
+      Object.assign(handler.context, { db: dbInstance })
+      if (secretsPath && removeSecrets) {
+        delete handler.context[secretsPath]
+      }
+      return next()
+    },
+
+    after: cleanup,
+    onError: cleanup
+  }
+}
