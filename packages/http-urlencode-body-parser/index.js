69e03fb4293ef313516150e098f229320f8f5dbf
will Farrell <willfarrell@users.noreply.github.com>
2019-12-11 07:00:56 -0700

    Dependency Performance Improvements (#442)
    * fix: move dep include out of middleware functions
    
    * fix: only include relevent clients
    
    * version bump

--- packages/http-urlencode-body-parser/index.js
+++ packages/http-urlencode-body-parser/index.js
@@ -4,23 +4,23 @@ const defaults = {
   extended: false
 }
 
-module.exports = (opts) => ({
-  before: (handler, next) => {
-    const options = Object.assign({}, defaults, opts)
+module.exports = (opts) => {
+  const options = Object.assign({}, defaults, opts)
+  const parserFn = options.extended ? require('qs').parse : require('querystring').decode
+  return {
+    before: (handler, next) => {
+      if (handler.event.headers) {
+        const contentTypeHeader = handler.event.headers['content-type'] || handler.event.headers['Content-Type']
+        if (contentTypeHeader) {
+          const { type } = contentType.parse(contentTypeHeader)
 
-    const parserFn = options.extended ? require('qs').parse : require('querystring').decode
-
-    if (handler.event.headers) {
-      const contentTypeHeader = handler.event.headers['content-type'] || handler.event.headers['Content-Type']
-      if (contentTypeHeader) {
-        const { type } = contentType.parse(contentTypeHeader)
-
-        if (type === 'application/x-www-form-urlencoded') {
-          handler.event.body = parserFn(handler.event.body)
+          if (type === 'application/x-www-form-urlencoded') {
+            handler.event.body = parserFn(handler.event.body)
+          }
         }
       }
-    }
 
-    next()
+      next()
+    }
   }
-})
+}
