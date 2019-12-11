338ed037a83b0ae46af3ed5b94bedea85fd6156a
will Farrell <willfarrell@users.noreply.github.com>
2019-12-11 07:28:23 -0700

    Port cache middleware chages from #359 to 1.0.0-alpha (#444)
    * fix: add in missing catch
    
    Closes #377
    
    * version bump

--- packages/cache/__tests__/index.js
+++ packages/cache/__tests__/index.js
@@ -52,4 +52,71 @@ describe('💽 Cache stuff', () => {
     expect(response).toEqual(response2)
     expect(originalHandler.mock.calls.length).toBe(1)
   })
+
+  test('It should cache things using custom cache settings and discard result of a resolve promised with setValue', async () => {
+    const calculateCacheId = (event) => Promise.resolve(event.id)
+    const myStorage = {}
+    const getValue = (key) => new Promise((resolve, reject) => {
+      setTimeout(() => resolve(myStorage[key]), 50)
+    })
+    const setValue = (key, value) => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        myStorage[key] = value
+        return resolve('Value to discard')
+      }, 50)
+    })
+
+    const originalHandler = jest.fn((event, context, cb) => {
+      cb(null, event.a + event.b)
+    })
+
+    const handler = middy(originalHandler)
+      .use(cache({
+        calculateCacheId,
+        getValue,
+        setValue
+      }))
+
+    const event = { id: 'some_unique_id', a: 2, b: 3 }
+
+    const response = await invoke(handler, event)
+    const response2 = await invoke(handler, event)
+    expect(response).toEqual(response2)
+    expect(originalHandler.mock.calls.length).toBe(1)
+  })
+
+  test('It should throw en error if setValue return a rejected promise', async () => {
+    const errMessage = 'Something bad happened'
+    const calculateCacheId = (event) => Promise.resolve(event.id)
+    const myStorage = {}
+    const getValue = (key) => new Promise((resolve, reject) => {
+      setTimeout(() => resolve(myStorage[key]), 50)
+    })
+    const setValue = (key, value) => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        myStorage[key] = value
+        return reject(new Error(errMessage))
+      }, 50)
+    })
+
+    const originalHandler = jest.fn((event, context, cb) => {
+      cb(null, event.a + event.b)
+    })
+
+    const handler = middy(originalHandler)
+      .use(cache({
+        calculateCacheId,
+        getValue,
+        setValue
+      }))
+
+    const event = { id: 'some_unique_id', a: 2, b: 3 }
+
+    try {
+      await invoke(handler, event)
+    } catch (e) {
+      expect(e.message).toEqual(errMessage)
+      expect(originalHandler.mock.calls.length).toBe(1)
+    }
+  })
 })
