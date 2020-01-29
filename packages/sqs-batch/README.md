29f5cdbf011b1f0470c9403d67fbb1079ea20b1a
Brett Andrews <brett.j.andrews@gmail.com>
2020-01-28 18:37:07 -0600

    feat: add sqs-batch middleware
    Related issue: #467

---
+++ packages/sqs-batch/README.md
@@ -0,0 +1,90 @@
+# Middy sqs batch middleware
+
+<div align="center">
+  <img alt="Middy logo" src="https://raw.githubusercontent.com/middyjs/middy/master/img/middy-logo.png"/>
+</div>
+
+<div align="center">
+  <p><strong>SQS batch middleware for the middy framework, the stylish Node.js middleware engine for AWS Lambda</strong></p>
+</div>
+
+<div align="center">
+<p>
+  <a href="http://badge.fury.io/js/%40middy%2Fsqs-batch">
+    <img src="https://badge.fury.io/js/%40middy%2Fsqs-batch.svg" alt="npm version" style="max-width:100%;">
+  </a>
+  <a href="https://snyk.io/test/github/middyjs/middy">
+    <img src="https://snyk.io/test/github/middyjs/middy/badge.svg" alt="Known Vulnerabilities" data-canonical-src="https://snyk.io/test/github/middyjs/middy" style="max-width:100%;">
+  </a>
+  <a href="https://standardjs.com/">
+    <img src="https://img.shields.io/badge/code_style-standard-brightgreen.svg" alt="Standard Code Style"  style="max-width:100%;">
+  </a>
+  <a href="https://greenkeeper.io/">
+    <img src="https://badges.greenkeeper.io/middyjs/middy.svg" alt="Greenkeeper badge"  style="max-width:100%;">
+  </a>
+  <a href="https://gitter.im/middyjs/Lobby">
+    <img src="https://badges.gitter.im/gitterHQ/gitter.svg" alt="Chat on Gitter"  style="max-width:100%;">
+  </a>
+</p>
+</div>
+
+Middleware for handling partially failed SQS batches.
+
+## Install
+
+To install this middleware you can use NPM:
+
+```bash
+npm install --save @middy/sqs-batch
+```
+
+## Options
+
+ - `sqs`: an `AWS.SQS` instance for deleting successfully processed messages from the queue.
+
+
+## Sample usage
+
+```javascript
+const middy = require('@middy/core')
+const sqsBatch = require('@middy/sqs-batch')
+
+const originalHandler = (event, context, cb) => {
+  const recordPromises = Records.map(async (record, index) => { /* Custom message processing logic */ })
+  const settledRecords = await Promise.allSettled(recordPromises)
+  return settledRecords
+}
+
+const handler = middy(originalHandler)
+  .use(sqsBatch())
+```
+
+Your Lambda function requires permission to delete the message from the queue.
+
+```json
+{
+  "Effect": "Allow",
+  "Action": [
+    "sqs:DeleteMessage"
+  ],
+  "Resource": "arn:aws:sqs..."
+}
+```
+
+## Middy documentation and examples
+
+For more documentation and examples, refers to the main [Middy monorepo on GitHub](https://github.com/middyjs/middy) or [Middy official website](https://middy.js.org).
+
+
+## Contributing
+
+Everyone is very welcome to contribute to this repository. Feel free to [raise issues](https://github.com/middyjs/middy/issues) or to [submit Pull Requests](https://github.com/middyjs/middy/pulls).
+
+
+## License
+
+Licensed under [MIT License](LICENSE). Copyright (c) 2017-2018 Luciano Mammino and the [Middy team](https://github.com/middyjs/middy/graphs/contributors).
+
+<a href="https://app.fossa.io/projects/git%2Bgithub.com%2Fmiddyjs%2Fmiddy?ref=badge_large">
+  <img src="https://app.fossa.io/api/projects/git%2Bgithub.com%2Fmiddyjs%2Fmiddy.svg?type=large" alt="FOSSA Status"  style="max-width:100%;">
+</a>
---
+++ packages/sqs-batch/__tests__/index.js
@@ -0,0 +1,104 @@
+const SQS = require('aws-sdk/clients/sqs')
+const eventMocks = require('@serverless/event-mocks').default
+const middy = require('../../core')
+const sqsBatchMiddleware = require('../')
+
+// Mock SQS
+const mockDeleteMessageBatch = jest.fn()
+mockDeleteMessageBatch.mockImplementation(() => ({
+  async promise () { }
+}))
+
+const sqs = new SQS({ region: 'us-east-2' })
+sqs.deleteMessageBatch = mockDeleteMessageBatch
+
+beforeEach(() => {
+  mockDeleteMessageBatch.mockClear()
+})
+
+describe('📨 SQS batch', () => {
+  test('Should resolve when there are no failed messages', async () => {
+    const event = eventMocks(
+      'aws:sqs',
+      {
+        Records: [{
+          messageAttributes: {
+            resolveOrReject: {
+              stringValue: 'resolve'
+            }
+          },
+          body: ''
+        }]
+      }
+    )
+    const originalHandler = jest.fn(async () => {
+      const settledRecords = await Promise.allSettled(event.Records.map((r) => Promise.resolve(r)))
+      return settledRecords
+    })
+    const handler = middy(originalHandler)
+      .use(sqsBatchMiddleware({ sqs }))
+    await expect(handler(event)).resolves.toEqual([
+      {
+        status: 'fulfilled',
+        value: {
+          attributes: {
+            ApproximateFirstReceiveTimestamp: '1545082649185',
+            ApproximateReceiveCount: '1',
+            SenderId: 'AIDAIENQZJOLO23YVJ4VO',
+            SentTimestamp: '1545082649183'
+          },
+          awsRegion: 'us-east-2',
+          body: '',
+          eventSource: 'aws:sqs',
+          eventSourceARN: 'arn:aws:sqs:us-east-2:123456789012:my-queue',
+          md5OfBody: '098f6bcd4621d373cade4e832627b4f6',
+          messageAttributes: {
+            resolveOrReject: {
+              stringValue: 'resolve'
+            }
+          },
+          messageId: '059f36b4-87a3-44ab-83d2-661975830a7d',
+          receiptHandle: 'AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...'
+        }
+      }
+    ])
+  })
+  test('Should throw with failure reasons', async () => {
+    const event = eventMocks(
+      'aws:sqs',
+      {
+        Records: [{
+          receiptHandle: 'successfulMessageReceiptHandle',
+          messageAttributes: {
+            resolveOrReject: {
+              stringValue: 'resolve'
+            }
+          }
+        }, {
+          messageAttributes: {
+            resolveOrReject: {
+              stringValue: 'reject'
+            }
+          }
+        }]
+      }
+    )
+    const originalHandler = jest.fn(async () => {
+      const settledRecords = await Promise.allSettled(event.Records.map((r) => {
+        if (r.messageAttributes.resolveOrReject.stringValue === 'resolve') return Promise.resolve(r.messageId)
+        return Promise.reject(new Error('Error message...'))
+      }))
+      return settledRecords
+    })
+    const handler = middy(originalHandler)
+      .use(sqsBatchMiddleware({ sqs }))
+    await expect(handler(event)).rejects.toThrow('Error message...')
+    expect(mockDeleteMessageBatch).toHaveBeenCalledWith({
+      Entries: [{
+        Id: '0',
+        ReceiptHandle: 'successfulMessageReceiptHandle'
+      }],
+      QueueUrl: 'https://sqs.us-east-2.amazonaws.com/123456789012/my-queue'
+    })
+  })
+})
