de75a7c4cd0d04ee89ca47638b50b3988343e565
will Farrell <will.farrell@gmail.com>
2020-12-28 19:26:07 -0700

    fix: add in missing cors headers
    Fixes #245

--- packages/http-cors/index.js
+++ packages/http-cors/index.js
@@ -15,49 +15,71 @@ const getOrigin = (incomingOrigin, options) => {
 
 const defaults = {
   getOrigin,
-  origin: '*',
-  headers: null,
   credentials: false,
-  maxAge: null,
-  cacheControl: null
+  headers: false,
+  methods: false,
+  origin: '*',
+  origins: [],
+  exposeHeaders: false,
+  maxAge: false,
+  requestHeaders: false,
+  requestMethods: false,
+  cacheControl: false
 }
 
 export default (opts = {}) => {
   const options = Object.assign({}, defaults, opts)
   const httpCorsMiddlewareAfter = async (handler) => {
-    // TODO update to use optional chaining
-    // TODO add in Access-Control-Allow-Methods, check fo other headers
+
     if (Object.prototype.hasOwnProperty.call(handler.event, 'httpMethod')) {
       handler.response = handler.response || {}
       handler.response.headers = handler.response.headers || {}
 
+      const existingHeaders = Object.keys(handler.response.headers)
+
+      // Check if already setup the header Access-Control-Allow-Credentials
+      if (options.credentials && !existingHeaders.includes('Access-Control-Allow-Credentials')) {
+        options.credentials = String(options.credentials)
+      }
+
       // Check if already setup Access-Control-Allow-Headers
-      if (options.headers !== null && !Object.prototype.hasOwnProperty.call(handler.response.headers, 'Access-Control-Allow-Headers')) {
+      if (options.headers && !existingHeaders.includes( 'Access-Control-Allow-Headers')) {
         handler.response.headers['Access-Control-Allow-Headers'] = options.headers
       }
 
-      // Check if already setup the header Access-Control-Allow-Credentials
-      if (Object.prototype.hasOwnProperty.call(handler.response.headers, 'Access-Control-Allow-Credentials')) {
-        options.credentials = JSON.parse(handler.response.headers['Access-Control-Allow-Credentials'])
-      }
-
-      if (options.credentials) {
-        handler.response.headers['Access-Control-Allow-Credentials'] = String(options.credentials)
+      // Check if already setup Access-Control-Allow-Methods
+      if (options.methods && !existingHeaders.includes('Access-Control-Allow-Methods')) {
+        handler.response.headers['Access-Control-Allow-Methods'] = options.methods
       }
 
       // Check if already setup the header Access-Control-Allow-Origin
-      if (!Object.prototype.hasOwnProperty.call(handler.response.headers, 'Access-Control-Allow-Origin')) {
+      if (options.origin && !existingHeaders.includes( 'Access-Control-Allow-Origin')) {
         const headers = handler.event.headers || {}
         const incomingOrigin = headers.origin || headers.Origin
         handler.response.headers['Access-Control-Allow-Origin'] = options.getOrigin(incomingOrigin, options)
       }
 
-      if (options.maxAge && !Object.prototype.hasOwnProperty.call(handler.response.headers, 'Access-Control-Max-Age')) {
+      // Check if already setup Access-Control-Expose-Headers
+      if (options.exposeHeaders && !existingHeaders.includes( 'Access-Control-Expose-Headers')) {
+        handler.response.headers['Access-Control-Expose-Headers'] = options.exposeHeaders
+      }
+
+      if (options.maxAge && !existingHeaders.includes('Access-Control-Max-Age')) {
         handler.response.headers['Access-Control-Max-Age'] = String(options.maxAge)
       }
 
+      // Check if already setup Access-Control-Request-Headers
+      if (options.requestHeaders && !existingHeaders.includes( 'Access-Control-Request-Headers')) {
+        handler.response.headers['Access-Control-Request-Headers'] = options.requestHeaders
+      }
+
+      // Check if already setup Access-Control-Request-Methods
+      if (options?.requestMethods && !existingHeaders.includes('Access-Control-Request-Methods')) {
+        handler.response.headers['Access-Control-Request-Methods'] = options.requestMethods
+      }
+
       if (handler.event.httpMethod === 'OPTIONS') {
-        if (options.cacheControl && !Object.prototype.hasOwnProperty.call(handler.response.headers, 'Cache-Control')) {
+        if (options.cacheControl && !existingHeaders.includes( 'Cache-Control')) {
           handler.response.headers['Cache-Control'] = String(options.cacheControl)
         }
       }
