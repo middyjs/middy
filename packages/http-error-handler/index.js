d925eb2a5825d0b419fa557dcc63e1642c345bf9
will Farrell <will.farrell@gmail.com>
2020-12-29 10:27:29 -0700

    fix: update error handler to catch all errors

--- packages/http-error-handler/index.js
+++ packages/http-error-handler/index.js
@@ -1,28 +1,33 @@
 import { jsonSafeParse } from '../core/util.js'
 
 const defaults = {
-  logger: console.error
+  logger: console.error,
+  fallbackMessage: null
 }
 
 export default (opts = {}) => {
   const options = Object.assign({}, defaults, opts)
 
   const httpErrorHandlerMiddlewareOnError = async (handler) => {
-    // if there are a `statusCode` and an `error` field
-    // this is a valid http error object
-    if (handler.error.statusCode && handler.error.message) {
-      if (typeof options.logger === 'function') {
-        options.logger(handler.error)
-      }
+    if (!handler.error.message) return  // Should never happen
 
-      handler.response = {
-        statusCode: handler.error.statusCode,
-        body: jsonSafeParse(handler.error.message)
-      }
-      handler.response.headers['Content-Type'] = typeof handler.response.body === 'string'
-        ? 'plain/text'
-        : 'application/json'
+    if (typeof options.logger === 'function') {
+      options.logger(handler.error)
     }
+
+    if (!handler.error.statusCode && options.fallbackMessage) {
+      handler.error = {
+        statusCode: 500,
+        message: options.fallbackMessage
+      }
+    }
+
+    handler.response.statusCode = handler.error.statusCode
+    handler.response.body = jsonSafeParse(handler.error.message)
+    handler.response.headers['Content-Type'] = typeof handler.response.body === 'string'
+      ? 'plain/text'
+      : 'application/json'
+
   }
 
   return {
