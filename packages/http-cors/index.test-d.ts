27d9fab047df21f084f9d5f1c1e5521ea6c5a8fb
Luciano <lucianomammino@gmail.com>
2021-03-02 09:02:11 +0000

    Added typings and typing tests for http-cors middleware

---
+++ packages/http-cors/index.test-d.ts
@@ -0,0 +1,22 @@
+import { expectType } from 'tsd'
+import middy from '@middy/core'
+import httpCors from '.'
+
+// use with default options
+let middleware = httpCors()
+expectType<middy.MiddlewareObj>(middleware)
+
+// use with all options
+middleware = httpCors({
+  credentials: true, // Access-Control-Allow-Credentials
+  headers: 'X-Custom-Header, Upgrade-Insecure-Requests', // 'Access-Control-Allow-Headers',
+  methods: 'POST, GET, OPTIONS', // 'Access-Control-Allow-Methods'
+  origin: 'foo.bar.com',
+  origins: ['foo.bar.com', 'foo.baz.com'],
+  exposeHeaders: 'Content-Length, X-Kuma-Revision', // Access-Control-Expose-Headers
+  maxAge: 600, // Access-Control-Max-Age
+  requestHeaders: 'X-PINGOTHER, Content-Type', // Access-Control-Request-Headers
+  requestMethods: 'POST', // Access-Control-Request-Methods
+  cacheControl: 'proxy-revalidate' // Cache-Control
+})
+expectType<middy.MiddlewareObj>(middleware)
