4b2cf79300fac7b0c928028a33a577799538e055
Luciano <lucianomammino@gmail.com>
2021-03-02 09:24:33 +0000

    Added typings and typing tests for http-header-normalizer

--- packages/http-header-normalizer/index.d.ts
+++ packages/http-header-normalizer/index.d.ts
@@ -1,10 +1,10 @@
 import middy from '@middy/core'
 
-interface IHttpHeaderNormalizerOptions {
+interface Options {
   normalizeHeaderKey?: (key: string) => string
-  canonical?: Boolean
+  canonical?: boolean
 }
 
-declare const httpHeaderNormalizer: middy.Middleware<IHttpHeaderNormalizerOptions, any, any>
+declare function httpHeaderNormalizer (options?: Options): middy.MiddlewareObj
 
 export default httpHeaderNormalizer
