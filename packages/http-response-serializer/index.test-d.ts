f55800defa7ffde6c626c25a5df126f1793e4a0a
Luciano <lucianomammino@gmail.com>
2021-03-03 09:12:08 +0000

    Added typings for response-serializer

---
+++ packages/http-response-serializer/index.test-d.ts
@@ -0,0 +1,17 @@
+import { expectType } from 'tsd'
+import middy from '@middy/core'
+import httpResponseSerializer from '.'
+
+// use with default options
+let middleware = httpResponseSerializer()
+expectType<middy.MiddlewareObj>(middleware)
+
+// use with all options
+middleware = httpResponseSerializer({
+  default: 'application/json',
+  serializers: [{
+    regex: /^application\/xml$/,
+    serializer: (data) => data
+  }]
+})
+expectType<middy.MiddlewareObj>(middleware)
