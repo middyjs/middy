0243cd5e4af2f9a579245d87929544b19e513708
Luciano <lucianomammino@gmail.com>
2021-03-06 18:15:24 +0000

    Added typings for s3-key-normalizer

--- packages/s3-key-normalizer/index.d.ts
+++ packages/s3-key-normalizer/index.d.ts
@@ -1,5 +1,5 @@
 import middy from '@middy/core'
 
-declare const s3KeyNormalizer: middy.Middleware<any, any, any>
+declare function s3KeyNormalizer (): middy.MiddlewareObj
 
 export default s3KeyNormalizer
