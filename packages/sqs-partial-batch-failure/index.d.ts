244bc8c22f52a1a3891f2cdf77cf4ee003aee3c1
Luciano <lucianomammino@gmail.com>
2021-03-06 19:10:33 +0000

    Added typings for sqs-partial-batch-failure

--- packages/sqs-partial-batch-failure/index.d.ts
+++ packages/sqs-partial-batch-failure/index.d.ts
@@ -1,15 +1,15 @@
 import middy from '@middy/core'
-import SQS from 'aws-sdk/clients/sqs.js'
-import { captuteAWSClient } from 'aws-xray-sdk'
+import { SQS } from 'aws-sdk'
+import { captureAWSClient } from 'aws-xray-sdk'
 
-interface ISQSPartialBatchFailureOptions {
-  AwsClient?: SQS
+interface Options {
+  AwsClient?: typeof SQS
   awsClientOptions?: Partial<SQS.Types.ClientConfiguration>
   awsClientAssumeRole?: string
-  awsClientCapture?: captuteAWSClient
+  awsClientCapture?: typeof captureAWSClient
   disablePrefetch?: boolean
 }
 
-declare const sqsPartialBatchFailure: middy.Middleware<ISQSPartialBatchFailureOptions, any, any>
+declare function sqsPartialBatchFailure (options?: Options): middy.MiddlewareObj
 
 export default sqsPartialBatchFailure
