3c8fafb2744b9db2495bc8242676f4b3fab704ce
will Farrell <will.farrell@gmail.com>
2021-03-19 22:01:16 -0600

    fix: add in ts files

---
+++ packages/s3-object-response/index.test-d.ts
@@ -0,0 +1,15 @@
+import { expectType } from 'tsd'
+import middy from '@middy/core'
+import { S3 } from 'aws-sdk'
+import { captureAWSClient } from 'aws-xray-sdk'
+import s3ObjectResponse from '.'
+
+// use with default options
+let middleware = s3ObjectResponse()
+expectType<middy.MiddlewareObj>(middleware)
+
+// use with all options
+middleware = s3ObjectResponse({
+  AwsClient: S3
+})
+expectType<middy.MiddlewareObj>(middleware)
