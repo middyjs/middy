83a5adbbdbdc74213e677a5d380f0adbf6dcccf2
will Farrell <will.farrell@gmail.com>
2021-04-10 17:46:00 -0600

    refactor: use optional chaining

--- packages/s3-key-normalizer/index.js
+++ packages/s3-key-normalizer/index.js
@@ -2,20 +2,18 @@ const normalizeKeyReplacePlus = /\+/g
 const normalizeKey = (key) => decodeURIComponent(key.replace(normalizeKeyReplacePlus, ' '))
 
 const s3KeyNormalizerMiddlewareBefore = async (request) => {
-  if (request.event.Records && Array.isArray(request.event.Records)) {
-    request.event.Records = request.event.Records.map((record) => {
-      const eventVersion = parseFloat(record.eventVersion)
-      if (
-        record.s3 &&
-        record.s3.object &&
-        eventVersion >= 2 &&
-        eventVersion < 3
-      ) {
-        record.s3.object.key = normalizeKey(record.s3.object.key)
-      }
-      return record
-    })
-  }
+  if (!Array.isArray(request.event?.Records)) return
+  request.event.Records = request.event.Records.map((record) => {
+    const eventVersion = Number.parseFloat(record.eventVersion)
+    if (
+      record.s3?.object &&
+      eventVersion >= 2 &&
+      eventVersion < 3
+    ) {
+      record.s3.object.key = normalizeKey(record.s3.object.key)
+    }
+    return record
+  })
 }
 
 const s3KeyNormalizerMiddleware = () => ({
