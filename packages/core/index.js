eb613260ca8614dab38ed0a411071b1b968ab1c5
Nicolás Fantone <nfantone@gmail.com>
2021-04-13 17:13:03 +0100

    feat(core): refactor to use koa style middlewares

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,124 +1,93 @@
-const middy = (handler = () => {}, plugin) => {
-  plugin?.beforePrefetch?.()
-  const beforeMiddlewares = []
-  const afterMiddlewares = []
-  const onErrorMiddlewares = []
+'use strict'
 
-  const instance = (event = {}, context = {}) => {
-    plugin?.requestStart?.()
-    const request = {
-      event,
-      context,
-      response: undefined,
-      error: undefined,
-      internal: {}
-    }
-
-    return runRequest(
-      request,
-      [...beforeMiddlewares],
-      handler,
-      [...afterMiddlewares],
-      [...onErrorMiddlewares],
-      plugin
-    )
+const assertIsFunction = (fn, message) => {
+  if (typeof fn !== 'function') {
+    throw new TypeError(message)
   }
+}
 
-  instance.use = (middlewares) => {
-    if (Array.isArray(middlewares)) {
-      for (const middleware of middlewares) {
-        instance.applyMiddleware(middleware)
+function compose(middlewares) {
+  return function runMiddlewares(request) {
+    let currentIndex = 0
+
+    async function dispatch(index) {
+      if (index < currentIndex) {
+        throw new Error('`next` was already called in this middleware')
       }
-      return instance
+
+      if (index < middlewares.length) {
+        const fn = middlewares[index]
+        currentIndex = index
+
+        function next() {
+          return dispatch(index + 1)
+        }
+
+        return fn(request, next)
+      }
     }
-    return instance.applyMiddleware(middlewares)
+
+    return dispatch(0)
   }
-
-  instance.applyMiddleware = (middleware) => {
-    const { before, after, onError } = middleware
-
-    if (!before && !after && !onError) {
-      throw new Error(
-        'Middleware must an object containing at least one key among "before", "after", "onError"'
-      )
-    }
-
-    if (before) instance.before(before)
-    if (after) instance.after(after)
-    if (onError) instance.onError(onError)
-
-    return instance
-  }
-
-  // Inline Middlewares
-  instance.before = (beforeMiddleware) => {
-    beforeMiddlewares.push(beforeMiddleware)
-    return instance
-  }
-  instance.after = (afterMiddleware) => {
-    afterMiddlewares.unshift(afterMiddleware)
-    return instance
-  }
-  instance.onError = (onErrorMiddleware) => {
-    onErrorMiddlewares.push(onErrorMiddleware)
-    return instance
-  }
-
-  instance.__middlewares = {
-    before: beforeMiddlewares,
-    after: afterMiddlewares,
-    onError: onErrorMiddlewares
-  }
-
-  return instance
 }
 
-const runRequest = async (
-  request,
-  beforeMiddlewares,
-  handler,
-  afterMiddlewares,
-  onErrorMiddlewares,
-  plugin
-) => {
-  try {
-    await runMiddlewares(request, beforeMiddlewares, plugin)
-    // Check if before stack hasn't exit early
-    if (request.response === undefined) {
-      plugin?.beforeHandler?.()
-      request.response = await handler(request.event, request.context)
-      plugin?.afterHandler?.()
-      await runMiddlewares(request, afterMiddlewares, plugin)
-    }
-  } catch (e) {
-    // Reset response changes made by after stack before error thrown
-    request.response = undefined
-    request.error = e
-    try {
-      await runMiddlewares(request, onErrorMiddlewares, plugin)
-      // Catch if onError stack hasn't handled the error
-      if (request.response === undefined) throw request.error
-    } catch (e) {
-      // Save error that wasn't handled
-      e.originalError = request.error
-      request.error = e
-      throw request.error
-    }
-  } finally {
-    await plugin?.requestEnd?.()
-  }
-  return request.response
-}
+function middy() {
+  let errorHandler
+  const middlewares = []
 
-const runMiddlewares = async (request, middlewares, plugin) => {
-  for (const nextMiddleware of middlewares) {
-    plugin?.beforeMiddleware?.(nextMiddleware?.name)
-    const res = await nextMiddleware?.(request)
-    plugin?.afterMiddleware?.(nextMiddleware?.name)
-    // short circuit chaining and respond early
-    if (res !== undefined) {
-      request.response = res
-      return
+  return {
+    get middlewares() {
+      return [...middlewares]
+    },
+
+    use(middleware) {
+      assertIsFunction(middleware, '`middleware` must be a function')
+      middlewares.push(middleware)
+      return this
+    },
+
+    error(cb) {
+      assertIsFunction(cb, '`cb` must be a function')
+      errorHandler = cb
+      return this
+    },
+
+    lambda(handler) {
+      assertIsFunction(handler, '`handler` must be a function')
+
+      async function handlerMiddleware(request, next) {
+        request.response = await handler(request.event, request.context)
+        return next()
+      }
+
+      middlewares.push(handlerMiddleware)
+      return this
+    },
+
+    handler() {
+      const middlewaresFn = compose(middlewares)
+
+      async function handleEvent(event, context = {}) {
+        const request = {
+          event,
+          context,
+          response: undefined,
+          _internal: {}
+        }
+
+        try {
+          await middlewaresFn(request)
+          return request.response
+        } catch (err) {
+          if (errorHandler) {
+            return errorHandler(err, request)
+          }
+
+          throw err
+        }
+      }
+
+      return handleEvent
     }
   }
 }
---
+++ packages/eslint-config/README.md
@@ -0,0 +1,1 @@
+# `@middy/eslint-config`
