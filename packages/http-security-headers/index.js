8f21d2f061fdcb92ade2e50d7217ace880beae60
will Farrell <will.farrell@gmail.com>
2021-06-27 22:31:23 -0600

    fix: add in missing `?`

--- packages/http-security-headers/index.js
+++ packages/http-security-headers/index.js
@@ -134,7 +134,7 @@ const httpSecurityHeadersMiddleware = (opts = {}) => {
       request.response.headers = helmet[key](request.response.headers, config)
     })
 
-    if (request.response.headers?.['Content-Type'].includes('text/html')) {
+    if (request.response.headers?.['Content-Type']?.includes('text/html')) {
       Object.keys(helmetHtmlOnly).forEach((key) => {
         const config = { ...defaults[key], ...options[key] }
         request.response.headers = helmetHtmlOnly[key](
