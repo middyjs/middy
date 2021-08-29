93f34022ffb0bff5eb92dc0057b95a041133dde8
KillDozerX2 <himanshupant127@gmail.com>
2021-08-30 03:01:53 +0530

    test: :white_check_mark: Added test for the middleware
    This is my first time writing a test with Ava, might have missed something crucial

--- packages/s3-download-record/__tests__/index.js
+++ packages/s3-download-record/__tests__/index.js
@@ -5,29 +5,30 @@ const test = require('ava')
 const sinon = require('sinon')
 const rewire = require('rewire')
 const middy = require('../../core/index.js')
-const { clearCache } = require('../../util')
 const S3 = require('aws-sdk/clients/s3.js') // v2
 // const { S3 } = require('@aws-sdk/client-s3') // v3
-const s3ObejctResponse = rewire('../index.js')
+const s3DownloadRecord = rewire('../index.js')
 
-let sandbox
-test.beforeEach((t) => {
-  sandbox = sinon.createSandbox()
-})
+/**
+ * @type {sinon.SinonSandbox}
+ */
+let sandbox = sinon.createSandbox()
 
-test.afterEach((t) => {
-  sandbox.restore()
-  clearCache()
-})
-
-const mockService = (client, responseOne, responseTwo) => {
+/**
+ * @param {typeof S3} client
+ * @param {Array<String>} responses
+ */
+const mockService = (client, responses) => {
   // aws-sdk v2
   const mock = sandbox.stub()
-  mock.onFirstCall().returns({ promise: () => Promise.resolve(responseOne) })
-  if (responseTwo) {
-    mock.onSecondCall().returns({ promise: () => Promise.resolve(responseTwo) })
-  }
-  client.prototype.writeGetObjectResponse = mock
+  responses.forEach((value, index) => {
+    mock.onCall(index).returns({
+      promise: () => Promise.resolve({
+        Body: value
+      })
+    })
+  })
+  client.prototype.getObject = mock;
   // aws-sdk v3
   // const mock = sandbox.stub(client.prototype, 'writeGetObjectResponse')
   // mock.onFirstCall().resolves(responseOne)
@@ -36,71 +37,47 @@ const mockService = (client, responseOne, responseTwo) => {
   return mock
 }
 
-const mockHttps = (mockResponse) => {
-  const mockStream = new PassThrough()
-  mockStream.push(mockResponse)
-  mockStream.end()
-
-  https.request = () => mockStream
-
-  sinon.spy(mockStream, 'pipe')
-  return https
-}
-
-const isReadableStream = (body) => {
-  return body instanceof eventEmitter && typeof body.read === 'function'
-}
-
-const event = {
-  getObjectContext: {
-    inputS3Url: 'https://s3.amazonservices.com/key?signature',
-    outputRoute: 'https://s3.amazonservices.com/key',
-    outputToken: 'token'
+const mockFs = {
+  promises: {
+    filesWritten: {},
+    async writeFile(path, data) {
+      this.filesWritten[path] = data;
+    }
   }
 }
 
-test.serial('It should pass a stream to handler', async (t) => {
-  mockService(S3, { statusCode: 200 })
-  s3ObejctResponse.__set__('https', mockHttps('hello world'))
+/**
+ * @type {import("aws-lambda").S3Event}
+ */
+const event = {
+  Records: [
+    {
+      s3: {
+        object: {
+          key: "obj1",
+        },
+        bucket: {
+          name: "bucket1"
+        }
+      }
+    }
+  ]
+}
 
+mockService(S3, [
+  "obj1Response1",
+])
+s3DownloadRecord.__set__('fs', mockFs)
+test.serial('It should download the first object along with default name', async (t) => {
   const handler = middy((event, context) => {
-    t.true(isReadableStream(context.s3Object))
-
-    return {
-      Body: context.s3Object
-    }
+    t.true(mockFs.promises.filesWritten["/tmp/bucket1/obj1"] === "obj1Response1")
   })
 
   handler.use(
-    s3ObejctResponse({
-      AwsClient: S3,
-      bodyType: 'stream'
+    s3DownloadRecord({
+      AwsClient: S3
     })
   )
 
   const response = await handler(event)
-  t.deepEqual(200, response.statusCode)
-})
-
-test.serial('It should pass a promise to handler', async (t) => {
-  mockService(S3, { statusCode: 200 })
-  s3ObejctResponse.__set__('https', mockHttps('hello world'))
-
-  const handler = middy(async (event, context) => {
-    t.true(typeof context.s3Object.then === 'function')
-
-    return {
-      Body: await context.s3Object
-    }
-  })
-
-  handler.use(
-    s3ObejctResponse({
-      AwsClient: S3,
-      bodyType: 'promise'
-    })
-  )
-
-  const response = await handler(event)
-  t.deepEqual(200, response.statusCode)
 })
