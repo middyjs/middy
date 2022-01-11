704b9b12905924e19795d7ef36263a28203b7c35
will Farrell <will.farrell@gmail.com>
2022-01-10 17:08:20 -0700

    fix: perf improvements

--- packages/event-normalizer/index.js
+++ packages/event-normalizer/index.js
@@ -11,37 +11,44 @@ const eventNormalizerMiddleware = () => {
   }
 }
 
-const normalizeS3KeyReplacePlus = /\+/g
 const parseEventRecords = (event) => {
   const records = event.Records ?? event.records // lowercase records is for Kinesis Firehose
   if (!Array.isArray(records)) return
 
+  let eventSource
   for (const record of records) {
-    if (record.EventSource === 'aws:sns') {
-      record.Sns.Message = jsonSafeParse(record.Sns.Message)
-      parseEventRecords(record.Sns.Message)
-    } else if (record.eventSource === 'aws:sqs') {
-      record.body = jsonSafeParse(record.body)
-      parseEventRecords(record.body)
-    } else if (record.eventSource === 'aws:dynamodb') {
-      record.dynamodb.Keys = unmarshall(record.dynamodb.Keys)
-      record.dynamodb.OldImage = unmarshall(record.dynamodb.OldImage)
-      record.dynamodb.NewImage = unmarshall(record.dynamodb.NewImage)
-    } else if (record.eventSource === 'aws:s3') {
-      record.s3.object.key = decodeURIComponent(
-        record.s3.object.key.replace(normalizeS3KeyReplacePlus, ' ')
-      )
-    } else if (record.eventSource === 'aws:kinesis') {
-      // Kinesis Stream
-      record.kinesis.data = jsonSafeParse(
-        Buffer.from(record.kinesis.data, 'base64').toString('utf-8')
-      )
-    } else if (record.kinesisRecordMetadata) {
-      // Kinesis Firehose
-      record.data = jsonSafeParse(
-        Buffer.from(record.data, 'base64').toString('utf-8')
-      )
-    }
+    // EventSource is for SNS
+    // deliveryStreamArn is for Kinesis Firehose
+    eventSource ??= record.eventSource ?? record.EventSource ?? event.deliveryStreamArn
+    parseEvent[eventSource]?.(record)
+  }
+}
+
+const normalizeS3KeyReplacePlus = /\+/g
+const parseEvent = {
+  'aws:dynamodb': (record) => {
+    record.dynamodb.Keys = unmarshall(record.dynamodb.Keys)
+    record.dynamodb.OldImage = unmarshall(record.dynamodb.OldImage)
+    record.dynamodb.NewImage = unmarshall(record.dynamodb.NewImage)
+  },
+  // Kinesis Stream
+  'aws:kinesis': (record) => {
+    record.kinesis.data = jsonSafeParse(Buffer.from(record.kinesis.data, 'base64').toString('utf-8'))
+  },
+  // Kinesis Firehose
+  'aws:lambda:events': (record) => {
+    record.data = jsonSafeParse(Buffer.from(record.data, 'base64').toString('utf-8'))
+  },
+  'aws:s3': (record) => {
+    record.s3.object.key = decodeURIComponent(record.s3.object.key.replace(normalizeS3KeyReplacePlus, ' '))
+  },
+  'aws:sns': (record) => {
+    record.Sns.Message = jsonSafeParse(record.Sns.Message)
+    parseEventRecords(record.Sns.Message)
+  },
+  'aws:sqs': (record) => {
+    record.body = jsonSafeParse(record.body)
+    parseEventRecords(record.body)
   }
 }
 
