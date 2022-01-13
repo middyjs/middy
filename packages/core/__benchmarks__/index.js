193996f5a90ec47c3d64ba75e18a26388b049f61
will Farrell <will.farrell@gmail.com>
2022-01-13 10:30:35 -0700

    fix: skip timeout in benchmark

--- packages/core/__benchmarks__/index.js
+++ packages/core/__benchmarks__/index.js
@@ -3,20 +3,31 @@ import middy from '../index.js'
 
 const suite = new Benchmark.Suite('@middy/core')
 
-const setupHandler = () => {
+const context = {
+  getRemainingTimeInMillis: () => 30000
+}
+const setupHandler = (timeoutEarlyInMillis = 0) => {
   const baseHandler = () => {}
-  return middy(baseHandler)
+  return middy(baseHandler, {timeoutEarlyInMillis})
 }
 
 const warmHandler = setupHandler()
+const warmtimeoutHandler = setupHandler(1000)
 
 suite
   .add('Cold Invocation', async (event = {}) => {
     const coldHandler = setupHandler()
-    await coldHandler(event)
+    await coldHandler(event, context)
+  })
+  .add('Cold Invocation w/ Timeout', async (event = {}) => {
+    const coldHandler = setupHandler(1000)
+    await coldHandler(event, context)
   })
   .add('Warm Invocation', async (event = {}) => {
-    await warmHandler(event)
+    await warmHandler(event, context)
+  })
+  .add('Warm Invocation with Timeout', async (event = {}) => {
+    await warmtimeoutHandler(event, context)
   })
   .on('cycle', (event) => {
     console.log(suite.name, String(event.target))
--- packages/event-normalizer/__benchmarks__/index.js
+++ packages/event-normalizer/__benchmarks__/index.js
@@ -5,6 +5,9 @@ import middleware from '../index.js'
 
 const suite = new Benchmark.Suite('@middy/event-normalizer')
 
+const context = {
+  getRemainingTimeInMillis: () => 30000
+}
 const setupHandler = () => {
   const baseHandler = () => {}
   return middy(baseHandler)
@@ -29,22 +32,22 @@ deepJsonEvent.Records[0].body = JSON.stringify(snsEvent)
 suite
   .add('Cold Invocation', async (event = {}) => {
     const coldHandler = setupHandler()
-    await coldHandler(event)
+    await coldHandler(event, context)
   })
   .add('S3 Event', async (event = {...s3Event}) => {
-    await warmHandler(event)
+    await warmHandler(event, context)
   })
   .add('Shallow JSON (SQS) Event', async (event = {...sqsEvent}) => {
-    await warmHandler(event)
+    await warmHandler(event, context)
   })
   .add('Deep JSON (S3>SNS>SQS) Event', async (event = {...deepJsonEvent}) => {
-    await warmHandler(event)
+    await warmHandler(event, context)
   })
   .add('DynamoDB Event', async (event = {...dynamoEvent}) => {
-    await warmHandler(event)
+    await warmHandler(event, context)
   })
   .add('Kinesis Event', async (event = {...kinesisEvent}) => {
-    await warmHandler(event)
+    await warmHandler(event, context)
   })
   .on('cycle', (event) => {
     console.log(suite.name, String(event.target))
