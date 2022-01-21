f660d1b02b8fd52ae55c95fc9aea78322cf40f77
Nemanja Stojanovic <n.stojanovic035@gmail.com>
2022-01-20 22:12:56 -0500

    update the types

--- packages/http-json-body-parser/index.d.ts
+++ packages/http-json-body-parser/index.d.ts
@@ -1,7 +1,8 @@
 import middy from '@middy/core'
 
 interface Options {
-  reviver?: (key: string, value: any) => any
+  reviver?: (key: string, value: any) => any,
+  rejectMsg?: string
 }
 
 declare function jsonBodyParser (options?: Options): middy.MiddlewareObj
