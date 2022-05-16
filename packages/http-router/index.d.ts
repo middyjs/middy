2876a244ba7166dc712d5b990b465699c996d211
Andy <andyhowarthjsy@icloud.com>
2022-05-16 18:39:35 +0100

    feat(packages/http-router): adding typings and tests

--- packages/http-router/index.d.ts
+++ packages/http-router/index.d.ts
@@ -1,5 +1,18 @@
-// import middy from '@middy/core'
+import middy from '@middy/core'
+import {
+    APIGatewayProxyEvent,
+    APIGatewayProxyResult,
+    Handler as LambdaHandler
+} from 'aws-lambda'
 
-declare function httpRouterHandler (): any
+type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'OPTIONS';
+
+interface Route {
+    method: Method;
+    path: string;
+    handler: LambdaHandler<APIGatewayProxyEvent, APIGatewayProxyResult>;
+}
+
+declare function httpRouterHandler(routes: Route[]): middy.MiddlewareObj
 
 export default httpRouterHandler
