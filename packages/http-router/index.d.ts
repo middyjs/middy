9684e94b1ed0a367dd31ef7745d9f6368b5a6bf3
will Farrell <will.farrell@gmail.com>
2022-05-16 20:32:51 -0600

    fix: add in ANY method

--- packages/http-router/index.d.ts
+++ packages/http-router/index.d.ts
@@ -1,18 +1,18 @@
 import middy from '@middy/core'
 import {
-    APIGatewayProxyEvent,
-    APIGatewayProxyResult,
-    Handler as LambdaHandler
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Handler as LambdaHandler
 } from 'aws-lambda'
 
-type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'OPTIONS';
+type Method = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'OPTIONS' | 'ANY'
 
 interface Route {
-    method: Method;
-    path: string;
-    handler: LambdaHandler<APIGatewayProxyEvent, APIGatewayProxyResult>;
+  method: Method
+  path: string
+  handler: LambdaHandler<APIGatewayProxyEvent, APIGatewayProxyResult>
 }
 
-declare function httpRouterHandler(routes: Route[]): middy.MiddlewareObj
+declare function httpRouterHandler (routes: Route[]): middy.MiddlewareObj
 
 export default httpRouterHandler
--- packages/http-router/index.test-d.ts
+++ packages/http-router/index.test-d.ts
@@ -2,23 +2,23 @@ import { expectType } from 'tsd'
 import middy from '@middy/core'
 import httpRouterHandler from '.'
 import {
-    APIGatewayProxyEvent,
-    APIGatewayProxyResult,
-    Handler as LambdaHandler
-  } from 'aws-lambda'
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Handler as LambdaHandler
+} from 'aws-lambda'
 
 const lambdaHandler: LambdaHandler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (event) => {
-    return {
-      statusCode: 200,
-      body: `Hello from method ${event.httpMethod} on path ${event.path}`
-    }
+  return {
+    statusCode: 200,
+    body: 'Hello world'
   }
+}
 
 const middleware = httpRouterHandler([
-    {
-        'method': 'GET',
-        'path': '/',
-        handler: lambdaHandler
-    }
+  {
+    method: 'GET',
+    path: '/',
+    handler: lambdaHandler
+  }
 ])
 expectType<middy.MiddlewareObj>(middleware)
