6251ddfbb605831ef3f56c5024947fbcb61115ad
Nick Chiu <coding.nikazu@gmail.com>
2022-06-16 00:39:55 +0200

    fix cs

--- packages/http-multipart-body-parser/index.js
+++ packages/http-multipart-body-parser/index.js
@@ -54,7 +54,7 @@ const parseMultipartData = (event, options) => {
   return new Promise((resolve, reject) => {
     busboy
       .on('file', (fieldname, file, info) => {
-        const { filename, encoding, mimeType: mimetype } = info;
+        const { filename, encoding, mimeType: mimetype } = info
         const attachment = {
           filename,
           mimetype,
