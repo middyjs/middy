213fb8fffe2b654afe40b7b66b4b75dc04324cb5
Shai Katz <shaikatz@users.noreply.github.com>
2022-06-23 12:42:52 +0300

    Update index.d.ts

--- packages/sqs-partial-batch-failure/index.d.ts
+++ packages/sqs-partial-batch-failure/index.d.ts
@@ -1,5 +1,5 @@
 import middy from '@middy/core'
-import SQS from 'aws-sdk/clients/SQS'
+import SQS from 'aws-sdk/clients/sqs'
 import { Options as MiddyOptions } from '@middy/util'
 
 interface Options<S = SQS> extends Pick<MiddyOptions<S, SQS.Types.ClientConfiguration>,
