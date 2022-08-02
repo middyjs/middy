2cd08446e15a25b14d1cadc8c16a217edf9e8a98
Vicary A <vicary.archangel@member.mensa.org>
2022-08-02 15:36:31 +0800

    feat: add types to http-json-body-parser

--- packages/http-json-body-parser/index.d.ts
+++ packages/http-json-body-parser/index.d.ts
@@ -1,9 +1,18 @@
 import middy from '@middy/core'
+import { APIGatewayEvent } from 'aws-lambda'
+import { JsonValue } from 'type-fest'
 
 interface Options {
   reviver?: (key: string, value: any) => any
 }
 
-declare function jsonBodyParser (options?: Options): middy.MiddlewareObj
+export type Event = Omit<APIGatewayEvent, 'body'> & {
+  /**
+   * The body of the HTTP request.
+   */
+  body: JsonValue
+}
+
+declare function jsonBodyParser(options?: Options): middy.MiddlewareObj<Event>
 
 export default jsonBodyParser
