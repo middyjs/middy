80674e2443b60edaa53c140c5cf35d89182ba5d8
Vicary A <vicary.archangel@member.mensa.org>
2022-08-02 21:07:56 +0800

    feat: add types to sts

--- packages/sts/index.d.ts
+++ packages/sts/index.d.ts
@@ -1,13 +1,35 @@
-import STS from 'aws-sdk/clients/sts'
 import middy from '@middy/core'
 import { Options as MiddyOptions } from '@middy/util'
+import { Context as LambdaContext } from 'aws-lambda'
+import STS from 'aws-sdk/clients/sts'
 
 interface Options<S = STS>
-  extends Pick<MiddyOptions<S, STS.Types.ClientConfiguration>,
-  'AwsClient' | 'awsClientOptions' | 'awsClientCapture' |
-  'fetchData' | 'disablePrefetch' | 'cacheKey' | 'cacheExpiry' | 'setToContext'> {
-}
+  extends Pick<
+    MiddyOptions<S, STS.Types.ClientConfiguration>,
+    | 'AwsClient'
+    | 'awsClientOptions'
+    | 'awsClientCapture'
+    | 'fetchData'
+    | 'disablePrefetch'
+    | 'cacheKey'
+    | 'cacheExpiry'
+    | 'setToContext'
+  > {}
 
-declare function sts (options?: Options): middy.MiddlewareObj
+export type Context<TOptions extends Options | undefined> = TOptions extends {
+  setToContext: true
+}
+  ? LambdaContext &
+      Record<
+        keyof TOptions['fetchData'],
+        {
+          accessKeyId: STS.accessKeyIdType
+          secretAccessKey: STS.accessKeySecretType
+          sessionToken: STS.tokenType
+        }
+      >
+  : LambdaContext
+
+declare function sts(options?: Options): middy.MiddlewareObj
 
 export default sts
