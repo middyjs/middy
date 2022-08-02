3a3ebe179361b956c9b32373323294afc842ae97
Vicary A <vicary.archangel@member.mensa.org>
2022-08-02 14:27:45 +0800

    feat: add types to cloudwatch-metrics

--- packages/cloudwatch-metrics/index.d.ts
+++ packages/cloudwatch-metrics/index.d.ts
@@ -1,4 +1,6 @@
 import middy from '@middy/core'
+import { MetricsLogger } from 'aws-embedded-metrics'
+import type { Context } from 'aws-lambda'
 export { MetricsLogger } from 'aws-embedded-metrics'
 
 interface Options {
@@ -6,6 +8,13 @@ interface Options {
   dimensions?: Array<Record<string, string>>
 }
 
-declare function cloudwatchMetrics (options?: Options): middy.MiddlewareObj
+declare function cloudwatchMetrics(
+  options?: Options
+): middy.MiddlewareObj<
+  unknown,
+  any,
+  Error,
+  Context & { metrics: MetricsLogger }
+>
 
 export default cloudwatchMetrics
--- packages/cloudwatch-metrics/index.test-d.ts
+++ packages/cloudwatch-metrics/index.test-d.ts
@@ -1,14 +1,19 @@
-import { expectType } from 'tsd'
 import middy from '@middy/core'
-import cloudwatchMetrics from '.'
+import { Context } from 'aws-lambda'
+import { expectType } from 'tsd'
+import cloudwatchMetrics, { MetricsLogger } from '.'
 
 // use with default options
 let middleware = cloudwatchMetrics()
-expectType<middy.MiddlewareObj>(middleware)
+expectType<
+  middy.MiddlewareObj<unknown, any, any, Context & { metrics: MetricsLogger }>
+>(middleware)
 
 // use with all options
 middleware = cloudwatchMetrics({
   namespace: 'myApp',
   dimensions: [{ Action: 'Buy' }]
 })
-expectType<middy.MiddlewareObj>(middleware)
+expectType<
+  middy.MiddlewareObj<unknown, any, any, Context & { metrics: MetricsLogger }>
+>(middleware)
