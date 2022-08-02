24569a5c1511742afb2b0c436e8e30ae89d20ec5
Vicary A <vicary.archangel@member.mensa.org>
2022-08-02 16:30:47 +0800

    feat: add types to s3-object-response

--- packages/s3-object-response/index.d.ts
+++ packages/s3-object-response/index.d.ts
@@ -1,13 +1,31 @@
-import S3 from 'aws-sdk/clients/s3'
 import middy from '@middy/core'
 import { Options as MiddyOptions } from '@middy/util'
+import { Context } from 'aws-lambda'
+import S3 from 'aws-sdk/clients/s3'
+import { ClientRequest } from 'http'
 
 interface Options<S = S3>
-  extends Pick<MiddyOptions<S, S3.Types.ClientConfiguration>,
-  'AwsClient' | 'awsClientOptions' | 'awsClientAssumeRole' | 'awsClientCapture' | 'disablePrefetch'> {
+  extends Pick<
+    MiddyOptions<S, S3.Types.ClientConfiguration>,
+    | 'AwsClient'
+    | 'awsClientOptions'
+    | 'awsClientAssumeRole'
+    | 'awsClientCapture'
+    | 'disablePrefetch'
+  > {
   bodyType?: 'stream' | 'promise'
 }
 
-declare function s3ObjectResponse (options?: Options): middy.MiddlewareObj
+export type S3ObjectContext<TOptions extends Options | undefined> = Context & {
+  s3Object: TOptions extends { bodyType: 'stream' }
+    ? ClientRequest
+    : TOptions extends { bodyType: 'promise' }
+    ? Promise<any>
+    : void
+}
+
+declare function s3ObjectResponse<TOptions extends Options | undefined>(
+  options?: TOptions
+): middy.MiddlewareObj<unknown, any, any, S3ObjectContext<TOptions>>
 
 export default s3ObjectResponse
