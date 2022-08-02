172764b8e5106544dae96af4f3b5aa70da65c09a
Vicary A <vicary.archangel@member.mensa.org>
2022-08-02 14:48:09 +0800

    feat: add types to http-event-normalizer

--- packages/http-event-normalizer/index.test-d.ts
+++ packages/http-event-normalizer/index.test-d.ts
@@ -1,11 +1,11 @@
-import { expectType } from 'tsd'
 import middy from '@middy/core'
-import httpEventNormalizer from '.'
+import { expectType } from 'tsd'
+import httpEventNormalizer, { Event } from '.'
 
 // use with default options
 let middleware = httpEventNormalizer()
-expectType<middy.MiddlewareObj>(middleware)
+expectType<middy.MiddlewareObj<Event>>(middleware)
 
 // use with all options
 middleware = httpEventNormalizer()
-expectType<middy.MiddlewareObj>(middleware)
+expectType<middy.MiddlewareObj<Event>>(middleware)
