e39783c27e495fb7c2b146e4de6889db3a321cc9
Vicary A <vicary.archangel@member.mensa.org>
2022-08-02 21:07:23 +0800

    feat: add types to ssm

--- packages/ssm/index.d.ts
+++ packages/ssm/index.d.ts
@@ -1,9 +1,26 @@
-import SSM from 'aws-sdk/clients/ssm'
-import { Options as MiddyOptions } from '@middy/util'
 import middy from '@middy/core'
+import { Options as MiddyOptions } from '@middy/util'
+import { Context as LambdaContext } from 'aws-lambda'
+import SSM from 'aws-sdk/clients/ssm'
+import { JsonValue } from 'type-fest'
 
-interface Options<S = SSM> extends MiddyOptions<S, SSM.Types.ClientConfiguration> {}
+interface Options<S = SSM>
+  extends MiddyOptions<S, SSM.Types.ClientConfiguration> {}
 
-declare function ssm (options?: Options): middy.MiddlewareObj
+type Basename<T> = T extends `${infer _P}/${infer _S}` ? Basename<_S> : T
+type ExtractPaths<T> = T extends `${infer _P}/${infer _S}` ? T : never
+type ExtractSingles<T> = T extends `${infer _P}/${infer _S}` ? never : T
+
+export type Context<TOptions extends Options | undefined> = TOptions extends {
+  setToContext: true
+}
+  ? LambdaContext &
+      Record<Basename<ExtractPaths<keyof TOptions['fetchData']>>, JsonValue> &
+      Record<ExtractSingles<keyof TOptions['fetchData']>, JsonValue>
+  : LambdaContext
+
+declare function ssm<TOptions extends Options>(
+  options?: TOptions
+): middy.MiddlewareObj<unknown, any, any, Context<TOptions>>
 
 export default ssm
--- packages/ssm/index.test-d.ts
+++ packages/ssm/index.test-d.ts
@@ -1,15 +1,14 @@
-import { expectType } from 'tsd'
 import middy from '@middy/core'
 import SSM from 'aws-sdk/clients/ssm'
 import { captureAWSClient } from 'aws-xray-sdk'
-import ssm from '.'
+import { expectType } from 'tsd'
+import ssm, { Context } from '.'
 
 // use with default options
-let middleware = ssm()
-expectType<middy.MiddlewareObj>(middleware)
+expectType<middy.MiddlewareObj<unknown, any, Error, Context<undefined>>>(ssm())
 
 // use with all options
-middleware = ssm({
+const options = {
   AwsClient: SSM,
   awsClientOptions: {
     secretAccessKey: 'abc'
@@ -17,5 +16,7 @@ middleware = ssm({
   awsClientAssumeRole: 'some-role',
   awsClientCapture: captureAWSClient,
   disablePrefetch: true
-})
-expectType<middy.MiddlewareObj>(middleware)
+}
+expectType<middy.MiddlewareObj<unknown, any, Error, Context<typeof options>>>(
+  ssm(options)
+)
