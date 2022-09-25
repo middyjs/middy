6018eaead2f1a2a630b4099975cff906766715d5
will Farrell <will.farrell@gmail.com>
2022-09-25 12:52:19 -0400

    ci: remove unused deps

--- packages/sqs-partial-batch-failure/index.test-d.ts
+++ packages/sqs-partial-batch-failure/index.test-d.ts
@@ -1,7 +1,5 @@
 import { expectType } from 'tsd'
 import middy from '@middy/core'
-import { SQSClient } from '@aws-sdk/client-sqs'
-import { captureAWSClient } from 'aws-xray-sdk'
 import sqsPartialBatchFailure from '.'
 
 // use with default options
