50f1cde843c1351c24bd0a92d80771156afada62
will Farrell <willfarrell@proton.me>
2022-10-11 08:28:27 -0600

    ci: improve naming

--- packages/secrets-manager/index.d.ts
+++ packages/secrets-manager/index.d.ts
@@ -3,8 +3,11 @@ import { Options as MiddyOptions } from '@middy/util'
 import { Context as LambdaContext } from 'aws-lambda'
 import { SecretsManagerClient } from '@aws-sdk/client-secretsmanager'
 
-interface Options<SM = SecretsManagerClient>
-  extends MiddyOptions<SM, SecretsManagerClient.Types.ClientConfiguration> {}
+interface Options<AwsSecretsManagerClient = SecretsManagerClient>
+  extends MiddyOptions<
+    AwsSecretsManagerClient,
+    SecretsManagerClient.Types.ClientConfiguration
+  > {}
 
 export type Context<TOptions extends Options | undefined> = TOptions extends {
   setToContext: true
@@ -12,7 +15,7 @@ export type Context<TOptions extends Options | undefined> = TOptions extends {
   ? LambdaContext & Record<keyof TOptions['fetchData'], any>
   : LambdaContext
 
-declare function secretsManager<TOptions extends Options | undefined> (
+declare function secretsManager<TOptions extends Options | undefined>(
   options?: TOptions
 ): middy.MiddlewareObj<unknown, any, Error, Context<TOptions>>
 
