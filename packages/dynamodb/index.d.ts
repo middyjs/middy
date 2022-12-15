17dd7d8106fff7fc2824e40131c6f4c8f38e89a9
Naor Peled <thehecticbyte@gmail.com>
2022-12-16 01:31:31 +0200

    feat(types): export options

--- packages/dynamodb/index.d.ts
+++ packages/dynamodb/index.d.ts
@@ -3,14 +3,15 @@ import { Options as MiddyOptions } from '@middy/util'
 import { Context as LambdaContext } from 'aws-lambda'
 import { DynamoDBClient, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb'
 
-type Options<AwsDynamoDBClient = DynamoDBClient> = Omit<MiddyOptions<AwsDynamoDBClient, DynamoDBClientConfig>, 'fetchData'>
-&
-{
-  fetchData?: {
-    // TODO: add more precise type
-    [key: string]: Record<string, any>
+export type Options<AwsDynamoDBClient = DynamoDBClient> =
+  Omit<MiddyOptions<AwsDynamoDBClient, DynamoDBClientConfig>, 'fetchData'>
+  &
+  {
+    fetchData?: {
+      // TODO: add more precise type
+      [key: string]: Record<string, any>
+    }
   }
-}
 
 export type Context<TOptions extends Options | undefined> = TOptions extends {
   setToContext: true
