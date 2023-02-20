cc0ffa3f82b40f41dc8c92554aa28fd9074348e4
will Farrell <willfarrell@proton.me>
2023-02-19 20:09:36 -0700

    fix: add in missing type
    Closes #1007

--- packages/util/index.d.ts
+++ packages/util/index.d.ts
@@ -1,7 +1,7 @@
 import middy from '@middy/core'
 
 interface Options<Client, ClientOptions> {
-  AwsClient?: new(...args: any[]) => Client
+  AwsClient?: new (...args: any[]) => Client
   awsClientOptions?: Partial<ClientOptions>
   awsClientAssumeRole?: string
   awsClientCapture?: (service: Client) => Client
@@ -20,26 +20,50 @@ type HttpError = Error & {
   [key: number]: any
 }
 
-declare function createPrefetchClient<Client, ClientOptions> (options: Options<Client, ClientOptions>): Client
+declare function createPrefetchClient<Client, ClientOptions>(
+  options: Options<Client, ClientOptions>
+): Client
 
-declare function createClient<Client, ClientOptions> (options: Options<Client, ClientOptions>, request: middy.Request): Promise<Client>
+declare function createClient<Client, ClientOptions>(
+  options: Options<Client, ClientOptions>,
+  request: middy.Request
+): Promise<Client>
 
-declare function canPrefetch<Client, ClientOptions> (options: Options<Client, ClientOptions>): boolean
+declare function canPrefetch<Client, ClientOptions>(
+  options: Options<Client, ClientOptions>
+): boolean
 
-declare function getInternal (variables: any, request: middy.Request): Promise<any>
+declare function getInternal(
+  variables: any,
+  request: middy.Request
+): Promise<any>
 
-declare function sanitizeKey (key: string): string
+declare function sanitizeKey(key: string): string
 
-declare function processCache<Client, ClientOptions> (options: Options<Client, ClientOptions>, fetch: (request: middy.Request) => any, request?: middy.Request): { value: any, expiry: number }
+declare function processCache<Client, ClientOptions>(
+  options: Options<Client, ClientOptions>,
+  fetch: (request: middy.Request, cachedValues: any) => any,
+  request?: middy.Request
+): { value: any; expiry: number }
 
-declare function getCache (keys: string): any
+declare function getCache(keys: string): any
 
-declare function clearCache (keys?: string | string[] | null): void
+declare function clearCache(keys?: string | string[] | null): void
 
-declare function jsonSafeParse (string: string, reviver?: (key: string, value: any) => any): any
+declare function jsonSafeParse(
+  string: string,
+  reviver?: (key: string, value: any) => any
+): any
 
-declare function normalizeHttpResponse (request: any, fallbackResponse?: any): any
+declare function normalizeHttpResponse(
+  request: any,
+  fallbackResponse?: any
+): any
 
-declare function createError (code: number, message: string, properties?: Record<string, any>): HttpError
+declare function createError(
+  code: number,
+  message: string,
+  properties?: Record<string, any>
+): HttpError
 
-declare function modifyCache (cacheKey: string, value: any): void
+declare function modifyCache(cacheKey: string, value: any): void
