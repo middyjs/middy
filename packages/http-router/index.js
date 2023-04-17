fa17e42002df59a6c74746af4b001485c99f94c4
ChouAndy <chouandy625@gmail.com>
2023-04-17 23:53:52 +0800

    Revert "fix: simplify logic"
    This reverts commit 22bdc1e0bbb3107cbc24c87b3522bed02f63d36f.

--- packages/http-router/index.js
+++ packages/http-router/index.js
@@ -59,7 +59,17 @@ const httpRouteHandler = (routes) => {
 const regexpDynamicWildcards = /\/\{(proxy)\+\}$/
 const regexpDynamicParameters = /\/\{([^/]+)\}/g
 
-const methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'] // ANY excluded by design
+export const Method = {
+  Get: 'GET',
+  Post: 'POST',
+  Put: 'PUT',
+  Patch: 'PATCH',
+  Delete: 'DELETE',
+  Options: 'OPTIONS',
+  Head: 'HEAD',
+  Any: 'ANY'
+}
+const methods = Object.values(Method).filter((method) => method !== 'ANY')
 
 const attachStaticRoute = (method, path, handler, routesType) => {
   if (method === 'ANY') {
