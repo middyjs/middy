a2ab8d73e72edebdbb6c816d657cbaaf2f7304b9
Luciano Mammino <lucianomammino@gmail.com>
2023-09-29 11:38:59 +0100

    feat(s3): improved types

--- packages/s3/index.d.ts
+++ packages/s3/index.d.ts
@@ -3,23 +3,43 @@ import { Options as MiddyOptions } from '@middy/util'
 import { Context as LambdaContext } from 'aws-lambda'
 import { GetObjectCommandInput, S3Client, S3ClientConfig } from '@aws-sdk/client-s3'
 
-export type Options<AwsS3Client = S3Client> = Omit<MiddyOptions<AwsS3Client, S3ClientConfig>, 'fetchData'>
+export type GetObjectCommandInputNoChecksumMode = Omit<GetObjectCommandInput, 'ChecksumMode'> & {
+  ChecksumMode?: never
+}
+export type ParamType<T> = GetObjectCommandInputNoChecksumMode & { __returnType?: T }
+export declare function s3Req<T> (req: GetObjectCommandInputNoChecksumMode): ParamType<T>
+
+export type S3Options<AwsS3Client = S3Client> = Omit<MiddyOptions<AwsS3Client, S3ClientConfig>, 'fetchData'>
 & {
   fetchData?: {
-    [key: string]: Omit<GetObjectCommandInput, 'ChecksumMode'> & {
-      ChecksumMode?: never
-    }
+    [key: string]: GetObjectCommandInputNoChecksumMode | ParamType<unknown>
   }
 }
 
-export type Context<TOptions extends Options | undefined> = TOptions extends {
-  setToContext: true
-}
-  ? LambdaContext & Record<keyof TOptions['fetchData'], any>
+export type Context<TOptions extends S3Options | undefined> =
+TOptions extends { setToContext: true }
+  ? TOptions extends { fetchData: infer TFetchData }
+    ? LambdaContext & {
+      [Key in keyof TFetchData]: TFetchData[Key] extends ParamType<infer T>
+        ? T
+        : unknown
+    }
+    : never
   : LambdaContext
 
-declare function s3Middleware<TOptions extends Options | undefined> (
+export type Internal<TOptions extends S3Options | undefined> =
+TOptions extends S3Options
+  ? TOptions extends { fetchData: infer TFetchData }
+    ? {
+        [Key in keyof TFetchData]: TFetchData[Key] extends ParamType<infer T>
+          ? T
+          : unknown
+      }
+    : {}
+  : {}
+
+declare function s3Middleware<TOptions extends S3Options | undefined> (
   options?: TOptions
-): middy.MiddlewareObj<unknown, any, Error, Context<TOptions>>
+): middy.MiddlewareObj<unknown, any, Error, Context<TOptions>, Internal<TOptions>>
 
 export default s3Middleware
--- website/docs/middlewares/dynamodb.md
+++ website/docs/middlewares/dynamodb.md
@@ -68,9 +68,9 @@ To exclude `@aws-sdk` add `@aws-sdk/client-dynamodb` to the exclude list.
 
 Data in DynamoDB can be stored as arbitrary structured data. It's not possible to know in advance what shape the fetched data will have, so by default the fetched parameters will have type `Record<string, NativeAttributeValue>`.
 
-You can provide some type hints by leveraging the `dynamoDbReq` utility function. This function allows you to specify what's the expected type that will be fetched for every AppConfig request.
+You can provide some type hints by leveraging the `dynamoDbReq` utility function. This function allows you to specify what's the expected type that will be fetched for every DynamoDB request.
 
-The idea is that, for every request specified in the `fetchData` option, rather than just providing the parameter path as a string, you can wrap it in a `dynamoDbReq<ParamType>(config)` call. Internally, `dynamoDbReq` is a function that will return `config` as received, but it allows you to use generics to provide type hints for the expected fetched value type for that parameter.
+The idea is that, for every request specified in the `fetchData` option, rather than just providing the parameter configuration as an object, you can wrap it in a `dynamoDbReq<ParamType>(config)` call. Internally, `dynamoDbReq` is a function that will return `config` as received, but it allows you to use generics to provide type hints for the expected fetched value type for that request.
 
 This way TypeScript can understand how to treat the additional data attached to the context and stored in the internal storage.
 
