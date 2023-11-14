7cb665fd7a24b18cf6da8858c08a31acfe9bf90b
Rob Beal <robertbeal@users.noreply.github.com>
2023-11-14 13:26:01 +0000

    fix: don't throw errors in http-event-normalizer

--- packages/http-event-normalizer/index.js
+++ packages/http-event-normalizer/index.js
@@ -3,13 +3,6 @@ const httpEventNormalizerMiddleware = () => {
     const { event } = request
 
     const version = pickVersion(event)
-    const isHttpEvent = isVersionHttpEvent[version]?.(event)
-    if (!isHttpEvent) {
-      throw new Error('[http-event-normalizer] Unknown http event format')
-    }
-    // VPC Lattice is an http event, however uses a different notation
-    // - query_string_parameters
-    // - is_base64_encoded
 
     if (version === '1.0') {
       event.multiValueQueryStringParameters ??= {}
@@ -31,12 +24,6 @@ const httpEventNormalizerMiddleware = () => {
 const pickVersion = (event) => {
   // '1.0' is a safer default
   return event.version ?? (event.method ? 'vpc' : '1.0')
-}
-
-const isVersionHttpEvent = {
-  '1.0': (event) => typeof event.httpMethod !== 'undefined',
-  '2.0': (event) => typeof event.requestContext.http.method !== 'undefined',
-  vpc: (event) => typeof event.method !== 'undefined'
 }
 
 export default httpEventNormalizerMiddleware
