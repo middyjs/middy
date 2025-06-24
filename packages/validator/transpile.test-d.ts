ba596ff5e2731df7f10052e48daf80ff99076a05
Luca Del Puppo <luca@delpuppo.net>
2025-06-24 19:14:11 +0200

    feat: refactor type tests for appConfig middleware and update package.json scripts
    Signed-off-by: Luca Del Puppo <luca@delpuppo.net>

--- packages/validator/transpile.test-d.ts
+++ packages/validator/transpile.test-d.ts
@@ -1,4 +1,5 @@
 import { expectType } from "tsd";
+import { transpileLocale, transpileSchema } from "./transpile";
 
 const schema = transpileSchema({ type: "object" }, {});
 expectType<any>(schema);
