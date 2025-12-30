620fdf7c6275708e587e8b223ae6c3d4848267e3
will Farrell <willfarrell@proton.me>
2025-12-30 08:03:40 -0700

    test: update fetch mock
    Signed-off-by: will Farrell <willfarrell@proton.me>

--- packages/s3-object-response/index.fuzz.js
+++ packages/s3-object-response/index.fuzz.js
@@ -2,23 +2,27 @@ import { test } from "node:test";
 import { S3Client, WriteGetObjectResponseCommand } from "@aws-sdk/client-s3";
 import { mockClient } from "aws-sdk-client-mock";
 import fc from "fast-check";
+import { MockAgent, setGlobalDispatcher } from "undici";
 import middy from "../core/index.js";
 import middleware from "./index.js";
 
 mockClient(S3Client)
 	.on(WriteGetObjectResponseCommand)
 	.resolves({ statusCode: 200 });
-global.fetch = (url, request) => {
-	return Promise.resolve(
-		new Response("", {
-			status: 200,
-			statusText: "OK",
-			headers: new Headers({
-				"Content-Type": "application/json; charset=UTF-8",
-			}),
-		}),
-	);
-};
+
+const agent = new MockAgent();
+setGlobalDispatcher(agent);
+agent
+	.get("https://s3.amazonservices.com")
+	.intercept({
+		path: "/key?signature",
+		method: "GET",
+	})
+	.reply(200, "{}", {
+		headers: {
+			"Content-Type": "application/json; charset=UTF-8",
+		},
+	});
 
 const handler = middy((event) => event).use(
 	middleware({
