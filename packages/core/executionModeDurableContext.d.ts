70dac5a1d01740d2ed33ae6ce1badfd59f679afd
will Farrell <willfarrell@proton.me>
2026-02-17 10:44:04 -0700

    chore: version bump
    Signed-off-by: will Farrell <willfarrell@proton.me>

--- packages/core/executionModeDurableContext.d.ts
+++ packages/core/executionModeDurableContext.d.ts
@@ -1,2 +1,4 @@
+// Copyright 2017 - 2026 will Farrell, Luciano Mammino, and Middy contributors.
+// SPDX-License-Identifier: MIT
 export type PluginExecutionMode = () => void;
 export declare const executionModeDurableContext: PluginExecutionMode;
