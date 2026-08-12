4ef9b97a35bb8e81a38e8de8d7f8b7f34d93b673
will Farrell <willfarrell@proton.me>
2026-08-12 13:00:25 -0600

    chore: version bump
    Signed-off-by: will Farrell <willfarrell@proton.me>

--- websites/middy.js.org/src/hooks/tardisecMiddleware.js
+++ websites/middy.js.org/src/hooks/tardisecMiddleware.js
@@ -18,19 +18,21 @@ const tardisecMiddleware = async ({ event, resolve }) => {
 		}
 	}
 
+	response.headers.delete("X-Sveltekit-Page");
+
 	// Kit can't emit require-trusted-types-for without forcing a trusted-types
 	// allowlist at build time, so append it here when absent.
 	// error during build:
 	// [Error loading svelte.config.js: The `csp.directives['trusted-types']` option must include 'svelte-trusted-html']
-	const csp = response.headers.get("Content-Security-Policy");
-	if (csp && !csp.includes("require-trusted-types-for")) {
-		response.headers.set(
-			"Content-Security-Policy",
-			`${csp};require-trusted-types-for 'script'`,
-		);
+	for (const header of [
+		"Content-Security-Policy",
+		"Content-Security-Policy-Report-Only",
+	]) {
+		const csp = response.headers.get(header);
+		if (csp && !csp.includes("require-trusted-types-for")) {
+			response.headers.set(header, `${csp};require-trusted-types-for 'script'`);
+		}
 	}
-
-	response.headers.delete("X-Sveltekit-Page");
 
 	return response;
 };
